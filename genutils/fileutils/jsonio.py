9d078f1bf7778fa2117b934221fbcfabb21f12fb
Timotej Bernat <tibe3324@colorado.edu>
2023-08-10 18:47:45 -0600

    Utilities for dealing with files and pathlib Paths

---
+++ genutils/fileutils/jsonio.py
@@ -0,0 +1,101 @@
+'''Utilities for simplifying the loading and writing of various classes to JSON'''
+
+from typing import Any, Callable, Optional, Union
+from abc import ABC, abstractstaticmethod
+
+import json
+from pathlib import Path
+
+
+# JSON-SPECIFIC FUNCTIONS
+JSONSerializable = Union[str, bool, int, float, tuple, list, dict] 
+
+def append_to_json(json_path : Path, **kwargs) -> None:
+    '''Add an entry to an existing JSON file'''
+    with json_path.open('r') as json_file:
+        jdat = json.load(json_file)
+
+    jdat.update(**kwargs)
+
+    with json_path.open('w') as json_file:
+        jdat = json.checkpoint(jdat, json_file, indent=4)
+
+# JSON CLASSES
+class JSONifiable(ABC): # TODO : implement encode/decode methods as identity return by default, ditch abstract class behavior
+    '''Base class which allows a child class to have its attributes written to and from a JSON file on-disc between interpreter sessions
+    Children must implement how dict data (i.e. self.__dict__) is encoded to and decoded from JSON formatted dict'''
+
+    # JSON encoding and decoding
+    @abstractstaticmethod
+    def serialize_json_dict(unser_jdict : dict[Any, Any]) -> dict[str, JSONSerializable]:
+        '''For converting selfs __dict__ data into a form that can be serialized to JSON'''
+        pass
+    
+    @abstractstaticmethod
+    def unserialize_json_dict(ser_jdict : dict[str, JSONSerializable]) -> dict[Any, Any]:
+        '''For de-serializing JSON-compatible data into a form that the __init__method can accept'''
+        pass
+
+    # File I/O
+    def to_file(self, savepath : Path) -> None:
+        '''Store parameters in a JSON file on disc'''
+        assert(savepath.suffix == '.json')
+        with savepath.open('w') as dumpfile:
+            json.dump(self.__class__.serialize_json_dict(self.__dict__), dumpfile, indent=4)
+
+    @classmethod
+    def from_file(cls, loadpath : Path) -> 'JSONifiable':
+        assert(loadpath.suffix == '.json')
+        with loadpath.open('r') as loadfile:
+            params = json.load(loadfile, object_hook=cls.unserialize_json_dict)
+
+        return cls(**params)
+    
+    @staticmethod
+    def update_checkpoint(funct : Callable) -> Callable[[Any], Optional[Any]]: # NOTE : this deliberately doesn't have a "self" arg!
+        '''Decorator for updating the on-disc checkpoint file after a function updates a Polymer attribute'''
+        def update_fn(self, *args, **kwargs) -> Optional[Any]:
+            ret_val = funct(self, *args, **kwargs) # need temporary value so update call can be made before returning
+            self.to_file()
+            return ret_val
+        return update_fn
+
+class JSONDict(dict):
+    '''Dict subclass which also updates an underlying JSON file - effectively and on-disc dict
+    !NOTE! - JSON doesn't support non-string keys, so all keys given will be stringified - plan accordingly!'''
+    def __init__(self, json_path : Path, *args, **kwargs):
+        if isinstance(json_path, str):
+            json_path = Path(json_path) # make input arg a bit more flexible to str input from user end
+
+        if json_path.suffix != '.json':
+            raise ValueError(f'The path "{json_path}" does not point to a .json file')
+        
+        self.json_path : Path = json_path
+        if self.json_path.exists():
+            try:
+                kwargs.update(self._read_file(json_path))
+            except json.JSONDecodeError: # catches Paths which point to incorrectly formatted JSONs - TODO: revise terrible except-pass structure
+                pass
+        else:
+            self.json_path.touch()
+
+        super().__init__(*args, **kwargs)
+        self._update_file() # ensure file contains current contents post-init
+
+    @staticmethod
+    def _read_file(json_path : Path) -> dict:
+        with json_path.open('r') as file:
+            return json.load(file)        
+
+    def _update_file(self, indent : int=4):
+        '''Save current dict contents to JSON file'''
+        with self.json_path.open('w') as file:
+            json.dump(self, file, indent=indent)
+
+    def __setitem__(self, __key: str, __value: JSONSerializable) -> None:
+        super().__setitem__(__key, __value)
+        self._update_file()
+
+    def __delitem__(self, __key: str) -> None:
+        super().__delitem__(__key)
+        self._update_file()
---
+++ genutils/fileutils/pathutils.py
@@ -0,0 +1,58 @@
+'''Utilities for editing, augmenting, and querying Paths'''
+
+from typing import Union
+from pathlib import Path
+    
+
+# PATH PROPERTY FUNCTIONS (DON'T MODIFY ANYTHING)
+def dotless(path : Path) -> str:
+    '''Separate the dot from a SINGLE extension file suffix'''
+    path.suffix.split('.')[-1] 
+
+def is_empty(path : Path) -> bool:
+    '''Check if a directory is empty'''
+    assert(path.is_dir())
+    return list(path.iterdir()) == [] # can't use "len" for generators : TODO : make this more efficient (i.e. iteration-based) for large directories
+
+
+# PATH CONVERSION FUNCTIONS (FOR CHAGNING BETWEEN TYPES)
+def aspath(path : Union[str, Path]) -> Path:
+	'''Allow functions which expect Paths to also accept strings'''
+	if not isinstance(path, Path):
+		path = Path(path)
+	return path
+
+def asstrpath(strpath : Union[str, Path]) -> str:
+	'''Allow functions which expect strings paths to also accept Paths'''
+	if not isinstance(strpath, str):
+		strpath = str(strpath)
+	return strpath
+
+
+# PATH MODIFICATION FUNCTIONS (CHANGING THE STRUCTURE OF A PATH OBJECT)
+def default_suffix(path : Path, suffix : str) -> Path:
+    '''Asserts that a path has a suffix, appending a specified default suffix if none exists'''
+    if not path.suffix:
+        path = path.with_name(f'{path.stem}.{suffix}') # ensure charge params path has correct extension
+
+    return path
+
+def prepend_parent(path : Path, new_parent : Path) -> Path:
+    '''Prepends a parent tree to an existing path'''
+    return new_parent / path
+
+def detach_parent(path : Path, old_parent : Path) -> Path:
+    '''Cuts off a parent tree from an existing path'''
+    return path.relative_to(old_parent)
+
+def exchange_parent(path : Path, old_parent : Path, new_parent : Path) -> Path:
+    '''Exchanges the parent tree of a path for another parent tree'''
+    return prepend_parent(path=detach_parent(path, old_parent), new_parent=new_parent)
+
+def local_rename(path : Path, new_name : str) -> Path:
+    '''Performs file rename relative to the parent directory (NOT the cwd)'''
+    return path.rename(path.with_name(new_name))
+
+def local_restem(path : Path, new_stem : str) -> Path:
+    '''Performs file rename relative to the parent directory (NOT the cwd), preserving the extension of the original file'''
+    return path.rename(path.with_stem(new_stem))
