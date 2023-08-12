43f9a8112dc6858c68f4ff5265b57067323bced1
Timotej Bernat <tibe3324@colorado.edu>
2023-08-11 19:06:59 -0600

    Reimplemented RDObj member check with factory

--- rdutils/rdtypes.py
+++ rdutils/rdtypes.py
@@ -3,6 +3,7 @@
 from typing import Any, TypeAlias, Union
 from rdkit import Chem
 
+from ..genutils.typetools import _union_member_factory
 
 # DEFINING RDKit TYPES
 RDMol  = Chem.rdchem.Mol
@@ -10,9 +11,5 @@ RWMol  = Chem.rdchem.RWMol
 RDAtom = Chem.rdchem.Atom
 RDBond = Chem.rdchem.Bond
 
-RDObj : TypeAlias = Union(RDMol, RWMol, RDAtom, RDBond)
-
-# TYPECHECKING
-def isrdobj(var : Any) -> bool:
-	'''Check if a variable is and RDKit object'''
-	return isinstance(var, RDObj.__args__)
+RDObj : TypeAlias = Union[RDMol, RWMol, RDAtom, RDBond]
+isrdobj = _union_member_factory(RDObj)
