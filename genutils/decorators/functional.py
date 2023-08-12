bd58a6df35fc3e3554eeacda40cc6f12791f2ae3
Timotej Bernat <tibe3324@colorado.edu>
2023-08-11 19:04:55 -0600

    Added decorator for string-Path interchangeability

--- genutils/decorators/functional.py
+++ genutils/decorators/functional.py
@@ -1,9 +1,12 @@
 '''Decorators for modifying functions'''
 
-from typing import Callable, Optional
-from copy import deepcopy
+from typing import Callable, Optional, Union
 
-from ..typetools import O, Args, KWArgs
+from copy import deepcopy
+from pathlib import Path
+
+from ..typetools import O, T, Args, KWArgs
+from ..fileutils.pathutils import aspath, asstrpath
 
 
 def optional_in_place(funct : Callable[[O, Args, KWArgs], None]) -> Callable[[O, Args, bool, KWArgs], Optional[O]]:
@@ -18,3 +21,17 @@ def optional_in_place(funct : Callable[[O, Args, KWArgs], None]) -> Callable[[O,
             funct(copy_obj, *args, **kwargs) 
             return copy_obj # return the new object
     return in_place_wrapper
+
+def allow_string_paths(funct : Callable[[Path, Args, KWArgs], T]) -> Callable[[Union[Path, str], Args, KWArgs], T]:
+    '''Modifies a function which expects a Path as its first argument to also accept string-paths'''
+    def str_path_wrapper(flex_path : Union[str, Path], *args : Args, **kwargs : KWArgs) -> T:
+        '''First converts stringy paths into normal Paths, then executes the original function'''
+        return funct(aspath(flex_path), *args, **kwargs)
+    return str_path_wrapper
+
+def allow_pathlib_paths(funct : Callable[[str, Args, KWArgs], T]) -> Callable[[Union[Path, str], Args, KWArgs], T]:
+    '''Modifies a function which expects a string path as its first argument to also accept canonical pathlib Paths'''
+    def str_path_wrapper(flex_path : Union[str, Path], *args : Args, **kwargs : KWArgs) -> T:
+        '''First converts normal Paths into stringy paths, then executes the original function'''
+        return funct(asstrpath(flex_path), *args, **kwargs)
+    return str_path_wrapper
