203b3725338e6f79e6812acf9eff4b4df8240d2e
Timotej Bernat <tibe3324@colorado.edu>
2023-08-15 18:58:24 -0600

    Renamed to simply "ports"

--- rdutils/reactions/ports.py
+++ rdutils/reactions/ports.py
@@ -6,9 +6,14 @@ from ..labeling import atomwise
 from ...genutils.decorators.functional import optional_in_place
 
 
+# PORT INFO QUERY
 def is_port(atom : RDAtom) -> bool:
     '''Shorthand for deciding if an atom is a bonding site ("port")''' 
     return atom.GetAtomicNum() == 0
+
+def num_ports(rdmol : RDMol) -> int:
+    '''Counts the number of port atoms present in a Mol'''
+    return sum(1 for atom in rdmol.GetAtoms() if is_port(atom))
 
 def get_port_ids(rdmol : RDMol) -> list[int]:
     '''Get atom indices of port (i.e. wild *-type or undefined) atoms'''
@@ -21,6 +26,7 @@ def get_port_ids(rdmol : RDMol) -> list[int]:
 neighbor_ports     = atomwise._neighbor_factory_by_condition(    condition=is_port)
 has_neighbor_ports = atomwise._has_neighbor_factory_by_condition(condition=is_port)
 
+# PORT MODIFICATION
 @optional_in_place
 def hydrogenate_rdmol_ports(rdmol : RDMol) -> None:
     '''Replace all port atoms with hydrogens'''
