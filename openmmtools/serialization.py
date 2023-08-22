62fb6d00f7ffc95e5b5c9dcdd2586b0e7314ab22
Timotej Bernat <tibe3324@colorado.edu>
2023-08-22 15:09:15 -0600

    Module for reading and writing OpenMM Simulations on disc

---
+++ openmmtools/serialization.py
@@ -0,0 +1,43 @@
+'''For reading and writing OpenMM components to files'''
+
+from pathlib import Path
+
+from openmm import System, Context, State
+from openmm.app import Simulation
+from openmm import XmlSerializer
+
+
+# GENERIC PARAMETER SETS
+DEFAULT_STATE_PARAMS : dict[str, bool] = {
+    'getPositions'  : True,
+    'getVelocities' : True,
+    'getForces'     : False,
+    'getEnergy'     : True,
+    'getParameters' : True,
+    'getParameterDerivatives' : False,
+    'getIntegratorParameters' : False
+}
+
+# SERIALIZATION FUNCTIONS
+def serialize_state_and_sys(sim : Simulation, out_dir : Path, out_name : str, state_params : dict[str, bool]=DEFAULT_STATE_PARAMS) -> None:
+    '''For saving State and System info of a Simulation to disc'''
+    sim_dict = {
+        'system' : sim.system,
+        'state' : sim.context.getState(**state_params)
+    }
+    
+    for affix, save_data in sim_dict.items():
+        save_path = out_dir / f'{out_name}_{affix}.xml'
+        save_path.touch()
+
+        with save_path.open('w') as file:
+            file.write( XmlSerializer.serialize(save_data) )
+
+def apply_state_to_sim(context : Context, state : State) -> None:
+    '''For applying saved State data to an existing OpenMM Simulation'''
+    context.setPeriodicBoxVectors(*state.getPeriodicBoxVectors())
+    context.setPositions(state.getPositions())
+    context.setVelocities(state.getVelocities())
+    context.setTime(state.getTime())
+
+    context.reinitialize(preserveState=True)    
