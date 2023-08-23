52e135a66015342c4690159b09bdb16076fcc6b1
Timotej Bernat <tibe3324@colorado.edu>
2023-08-23 17:33:55 -0600

    Added list of functional group entries to match table

--- monomers/substruct/functgroups/__init__.py
+++ monomers/substruct/functgroups/__init__.py
@@ -10,18 +10,24 @@ _MODULE_PATH = Path(__file__).parent
 
 # load/generating functional group smarts table
 from . import _daylight_scrape
+from .records import FnGroupSMARTSEntry
 import pandas as pd
 
 
-_FN_GRP_TABLE_NAME : str = 'fn_group_smarts'
-FN_GRP_TABLE_PATH = _MODULE_PATH / f'{_FN_GRP_TABLE_NAME}.csv'
+_fgtab_name : str = 'fn_group_smarts'
+_fgtab_path = _MODULE_PATH / f'{_fgtab_name}.csv'
 
-if not FN_GRP_TABLE_PATH.exists(): # if data table is missing, scrape data back off of Daylight SMARTS sight and save
+if not _fgtab_path.exists(): # if data table is missing, scrape data back off of Daylight SMARTS sight and save
     LOGGER.warning(F'No functional group SMARTS data from LUT found on system; regenerating from {_daylight_scrape.DAYLIGHT_URL}')
     FN_GROUP_TABLE = _daylight_scrape.scrape_SMARTS()
     FN_GROUP_TABLE.sort_values('category', inplace=True)  # sort by category
     FN_GROUP_TABLE.reset_index(drop=True, inplace=True)   # renumber by new order
-    FN_GROUP_TABLE.to_csv(FN_GRP_TABLE_PATH, index=False) # save for future lookup
+    FN_GROUP_TABLE.to_csv(_fgtab_path, index=False) # save for future lookup
 else:
     LOGGER.info('Loading functional group SMARTS data from LUT')
-    FN_GROUP_TABLE = pd.read_csv(FN_GRP_TABLE_PATH) # otherwise, read data from table on import
+    FN_GROUP_TABLE = pd.read_csv(_fgtab_path) # otherwise, read data from table on import
+
+FN_GROUP_ENTRIES = [ # collate into list of easily queryable entries
+    FnGroupSMARTSEntry(**fgdict)
+        for fgdict in FN_GROUP_TABLE.to_dict(orient='records')
+]
