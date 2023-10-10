60cf95db4a156d7dd7ae811defdb6416f66ef10a
Timotej Bernat <tibe3324@colorado.edu>
2023-10-09 20:07:54 -0600

    Deferred box dim conversion to boxvectors flexible parser

--- openfftools/solvation/packing.py
+++ openfftools/solvation/packing.py
@@ -6,7 +6,8 @@ LOGGER = logging.Logger(__name__)
 from typing import Optional, Union
 
 import numpy as np
-from openmm.unit import Quantity, AVOGADRO_CONSTANT_NA
+from openmm.unit import Quantity
+
 from openff.toolkit import Topology, Molecule, ForceField
 from openff.interchange.components import _packmol as packmol
 
@@ -16,10 +17,8 @@ from . import boxvectors, physprops
 def pack_topology_with_solvent(offtop : Topology, solvent : Molecule, box_vecs : Union[boxvectors.VectorQuantity, boxvectors.BoxVectorsQuantity], density : Quantity, exclusion : Optional[Quantity]=None) -> Topology:
     '''Pack a Topology with a given solvent molecule to a given box size with desired density
     The dimensions of the desired box_dimensions '''
-    if box_vecs.ndim == 1:
-        box_vecs = boxvectors.xyz_to_box_vectors(box_vecs) # convert to 2D box vectors if only XYZ dimensions are provided
-
     # obtain bounding box (plus exclusion) for topology
+    box_vecs = boxvectors.box_vectors_flexible(box_vecs) # up-convert to full box vectors if only dimensions are given
     min_box_vecs = boxvectors.get_topology_bbox(offtop)
     if exclusion is not None:
         min_box_vecs = boxvectors.pad_box_vectors_uniform(min_box_vecs, exclusion)
