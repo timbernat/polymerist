2198cdcd9fc3f7e66d4f5bfa5a4ff45b923ae2be
Timotej Bernat <tibe3324@colorado.edu>
2023-12-01 17:25:48 -0700

    Added optional flag for map numbering in combined_rdmol

--- rdutils/amalgamation/bonding.py
+++ rdutils/amalgamation/bonding.py
@@ -30,10 +30,12 @@ def are_bonded_atoms(rdmol : RDMol, atom_id_1 : int, atom_id_2 : int) -> bool:
     '''Check if pair of atoms in an RDMol have a bond between then'''
     return (rdmol.GetBondBetweenAtoms(atom_id_1, atom_id_2) is not None)
 
-def combined_rdmol(*rdmols : Iterable[RDMol], editable : bool=True) -> Union[RDMol, RWMol]:
+def combined_rdmol(*rdmols : Iterable[RDMol], assign_map_nums : bool=True, editable : bool=True) -> Union[RDMol, RWMol]:
     '''Merge two RDMols into a single molecule with contiguous, non-overlapping atom map numbers'''
-    numbered_mols = molwise.assign_contiguous_atom_map_nums(*rdmols, in_place=False) 
-    combo = reduce(Chem.CombineMols, numbered_mols) # combine into single Mol object to allow for bonding
+    if assign_map_nums: # assign coniguous, unique atom map numbers
+        rdmols = molwise.assign_contiguous_atom_map_nums(*rdmols, in_place=False) 
+    
+    combo = reduce(Chem.CombineMols, rdmols) # combine into single Mol object to allow for bonding
 
     if editable:
         return Chem.RWMol(combo) # make combined Mol modifiable
