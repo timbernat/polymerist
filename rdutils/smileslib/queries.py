ee1165e11f55bbb7433ad735b7a6d940c3ce559c
Timotej Bernat <tibe3324@colorado.edu>
2023-12-07 18:36:45 -0700

    Added query Mol for heavy atoms which used to be linkers

--- rdutils/smileslib/queries.py
+++ rdutils/smileslib/queries.py
@@ -16,7 +16,12 @@ SPECIAL_SMARTS = {
         for query_label, symbol in _special.items()
 }
 
-SPECIAL_QUERY_MOLS = {
+SPECIAL_QUERY_MOLS = { # TODO : make these lambda-like so that a unique object is returned on access
     query_name : Chem.MolFromSmarts(smarts)
         for query_name, smarts in SPECIAL_SMARTS.items()
 }
+
+# TODO : add "magic" Prop queries (rxns, SDFile, and mapping)
+dummy_prop_query = rdqueries.HasPropQueryAtom('was_dummy') # heavy atom which was converted from a dummy atom in a reaction
+heavy_and_former_dummy = Chem.MolFromSmarts('A')
+heavy_and_former_dummy.GetAtomWithIdx(0).ExpandQuery(dummy_prop_query) # cast as Mol to allow for quick check via GetSubstructMatch
