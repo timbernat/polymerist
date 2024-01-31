18d26f46f16545bab2c5d08dd53274d280b3fd8f
Timotej Bernat <tibe3324@colorado.edu>
2024-01-30 19:28:09 -0700

    Separated automorphism counting into its own function

--- rdutils/smileslib/queries.py
+++ rdutils/smileslib/queries.py
@@ -31,11 +31,15 @@ def num_substruct_queries(target_mol : RDMol, substruct_query : RDMol, *args, **
     '''Get the number of RDKit substruct matches to a SMARTS query within a given target Mol'''
     return len(target_mol.GetSubstructMatches(substruct_query, *args, **kwargs)) # default "asMols=False" is fine here for length
 
+def num_automorphisms(substruct_query : RDMol, *args, **kwargs) -> int:
+    '''Get the matches a substructure query has to itself; provides measure of the degree of symmetry of the query'''
+    return num_substruct_queries(substruct_query, substruct_query, uniquify=False, *args, **kwargs) # !CRITICAL! that matches be non-unique
+
 def num_substruct_queries_distinct(target_mol : RDMol, substruct_query : RDMol) -> int: # TODO : also include stereochemical symmetries (via "useChirality" flag) 
     '''Get the number of distinct, non-overlapping RDKit substruct matches to a SMARTS query within a given target Mol
     Accounts for automorphic symmetries of the query to avoid double-counting distinct groups'''
     num_distinct : float = num_substruct_queries(target_mol, substruct_query, uniquify=False) # !CRITICAL! that matches be non-unique
-    num_distinct /= num_substruct_queries(  substruct_query, substruct_query, uniquify=False) # quotient out number of substructure automorphisms
+    num_distinct /= num_automorphisms(substruct_query) # quotient out number of substructure automorphisms
     if num_distinct.is_integer(): # should be guaranteed to be an integer by Lagrange's Theorem, but good to double check here
         return int(num_distinct)
     else:
