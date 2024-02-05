793ce2b6ea299b75bd46b55ced0d4d130f65f910
Timotej Bernat <tibe3324@colorado.edu>
2024-02-05 16:45:55 -0700

    Provided module-wide registration of NAGL Model v0.1.0-rc1

--- openfftools/__init__.py
+++ openfftools/__init__.py
@@ -12,15 +12,26 @@ FFDIR = Path(openforcefields.get_forcefield_dirs_paths()[0])
 from openff.toolkit import ToolkitRegistry
 from openff.toolkit import GLOBAL_TOOLKIT_REGISTRY as GTR
 
-## Register GNN Toolkits
+# Register OpenFf-compatible GNNs
+## Espaloma Toolkit Wrappers
 from espaloma_charge.openff_wrapper import EspalomaChargeToolkitWrapper
 GTR.register_toolkit(EspalomaChargeToolkitWrapper)
 
+## NAGL Toolkit Wrappers
 from openff.nagl.toolkits import NAGLRDKitToolkitWrapper, NAGLOpenEyeToolkitWrapper
 GTR.register_toolkit(NAGLRDKitToolkitWrapper)
 GTR.register_toolkit(NAGLOpenEyeToolkitWrapper)
 
-## Extract name-keyed TK wrappers and Registries
+## NAGL GNN Model
+from openff import nagl_models
+from openff.nagl import GNNModel
+
+NAGL_MODEL_PATH = nagl_models.list_available_nagl_models()[1] # Path(/home/timber/miniconda3/envs/polymerist-env/lib/python3.11/site-packages/openff/nagl_models/models/openff-gnn-am1bcc-0.1.0-rc.1.pt)
+NAGL_MODEL_PATH = nagl_models.validate_nagl_model_path(NAGL_MODEL_PATH) # double check that this model path is still one of the valid entry point
+NAGL_MODEL = GNNModel.load(NAGL_MODEL_PATH)
+
+
+# Separate all registered TK Wrappers and Registries by name
 TKWRAPPERS = { 
     tk_wrap.toolkit_name : tk_wrap
         for tk_wrap in GTR.registered_toolkits
