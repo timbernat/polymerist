0b3438656c7ecf21acde5b9e2c310da359034716
Timotej Bernat <tibe3324@colorado.edu>
2024-02-09 17:41:40 -0700

    Added SI-prefixed versions of eV Unit

--- unitutils/extraunits.py
+++ unitutils/extraunits.py
@@ -1,10 +1,13 @@
 '''Defining units which, for one reason or another, are not defined in Pint or OpenMM units'''
 
-from openmm.unit import Unit, ScaledUnit 
+import sys
+from openmm.unit import Unit, ScaledUnit
+from openmm.unit.prefix import define_prefixed_units
 
 # DEFINING ELECTRON VOLTS
 from openmm.unit import joule
-from scipy.constants import electron_volt as electron_volt_joules
+from scipy.constants import electron_volt as electron_volt_in_joules
 
-electronvolt_base = ScaledUnit(electron_volt_joules, joule, 'electronvolt', 'eV')
-electronvolt = eV = Unit({electronvolt_base : 1.0})
+electronvolt_base_unit = ScaledUnit(electron_volt_in_joules, joule, 'electronvolt', 'eV')
+electronvolt = eV = Unit({electronvolt_base_unit : 1.0})
+define_prefixed_units(electronvolt_base_unit, module=sys.modules[__name__])
