b57c3df084b80a025f351bb36782584033b0114f
Timotej Bernat <tibe3324@colorado.edu>
2024-02-09 17:05:09 -0700

    Broke unitutils script into subdivided module with dimensionality and MD interop submodules

--- unitutils/interop.py
+++ unitutils/interop.py
@@ -1,39 +1,17 @@
-'''Tools for unit checking'''
+'''Decorators for handling interconversion between the OpenMM and OpenFF (Pint) unit engines'''
 
-from typing import Any, Callable, Union, TypeVar
+from typing import Callable, TypeVar
 R = TypeVar('R')   # for representing generic return values
 Q = TypeVar('Q')   # for representing generic Quantity-like objects
 
 from pint import Quantity as PintQuantity # this is also the base class for all OpenFF-style units
-from openmm.unit import Quantity, Unit, length_dimension
+from openmm.unit import Quantity
 from openff.units.openmm import (
     from_openmm as openmm_to_openff,
     to_openmm as openff_to_openmm,
 )
 
 
-# CHECKING FOR AN REMOVING UNITS
-class MissingUnitsError(Exception):
-    pass
-
-def hasunits(obj : Any) -> bool:
-    '''Naive but effective way of checking for pint and openmm units'''
-    return any(hasattr(obj, attr) for attr in ('unit', 'units')) 
-
-def strip_units(coords : Union[tuple, PintQuantity, Quantity]) -> tuple[float]:
-    '''
-    Sanitize coordinate tuples for cases which require unitless quantities
-    Specifically needed since OpenMM and pint each have their own Quantity and Units classes
-    '''
-    if isinstance(coords, PintQuantity):
-        return coords.magnitude
-    elif isinstance(coords, Quantity):
-        return coords._value
-
-    return coords
-
-
-# DECORATORS FOR INTEROP BETWEEN OpenMM AND OpenFF UNIT SYSTEM
 def allow_openmm_units(funct : Callable[[Q], R]) -> Callable[[Q], R]:
     '''Allow a Callable which expects ALL of its args to be OpenFF Quanitities to also accept equivalent OpenMM Quanitites'''
     def wrapper(*args, **kwargs) -> R:
@@ -66,18 +44,3 @@ def allow_openff_units(funct : Callable[[Q], R]) -> Callable[[Q], R]:
         return funct(*new_args, **new_kwargs)
     return wrapper
 
-
-# CHECKING DIMENSIONALITY
-@allow_openff_units
-def is_volume(unit_val : Union[Unit, Quantity]) -> bool:
-    '''Return whether a unit corresponds to a volume'''
-    if isinstance(unit_val, Quantity):
-        unit_val = unit_val.unit # extract just the unit component if a Quantity is passed
-
-    for i, (dim, exp) in enumerate(unit_val.iter_base_dimensions()):
-        if i > 0:
-            return False # immediate rule out if more than just one unit is present
-        
-    if (dim == length_dimension) and (exp == 3.0): # if monodimensional, check that the single dimension is L^3
-        return True
-    return False
