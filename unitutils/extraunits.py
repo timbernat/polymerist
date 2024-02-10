ba50a3fb093c4ee6b59ec4a7f4fff44ebda45db3
Timotej Bernat <tibe3324@colorado.edu>
2024-02-09 19:03:59 -0700

    Added definition for statcoulombs

--- unitutils/extraunits.py
+++ unitutils/extraunits.py
@@ -3,7 +3,7 @@
 import sys
 _MODULE = sys.modules[__name__]
 
-from openmm.unit import Unit, ScaledUnit
+from openmm.unit import Unit, BaseUnit, ScaledUnit
 from openmm.unit.prefix import define_prefixed_units
 
 
@@ -16,9 +16,18 @@ electronvolt = eV = Unit({electronvolt_base_unit : 1.0})
 define_prefixed_units(electronvolt_base_unit, module=_MODULE)
 
 
-# VISCOSITY
+# CGS units
+## VISCOSITY
 from openmm.unit import centimeter, gram, second
 
 poise_base_unit = ScaledUnit(1.0, gram/(centimeter*second), 'poise', 'P')
 poise = Unit({poise_base_unit : 1.0})
 define_prefixed_units(poise_base_unit, module=_MODULE)
+
+## CHARGE
+from openmm.unit import charge_dimension, coulomb_base_unit
+from scipy.constants import c
+
+statcoulomb_base_unit = BaseUnit(charge_dimension, 'statcoulomb', 'statC') # TOSELF : evidently, this correspondence isn;t always exact
+statcoulomb_base_unit.define_conversion_factor_to(coulomb_base_unit, 1/(10*c)) # derived from the speed of light
+statcoulomb = Unit({statcoulomb_base_unit : 1.0})
