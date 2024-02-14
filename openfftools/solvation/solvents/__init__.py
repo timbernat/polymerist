a4464ca646f89e207007e2c867adb1d2412e02dd
Timotej Bernat <tibe3324@colorado.edu>
2024-02-14 14:44:15 -0700

    Removed OpenEye dependency for water_TIP3P.sdf export, remade sdf with RDKit

--- openfftools/solvation/solvents/__init__.py
+++ openfftools/solvation/solvents/__init__.py
@@ -11,7 +11,7 @@ from ... import TKREGS
 
 
 def generate_water_TIP3P() -> Molecule:
-    '''Helper method for creating a new TIP3p water representation from scratch'''
+    '''Helper method for creating a new TIP3P water representation from scratch'''
     TIP3P_ATOM_CHARGES = { # NOTE : units deliberately omitted here (become applied to entire charge array)
         'H' :  0.417,
         'O' : -0.843
@@ -31,7 +31,7 @@ _water_path = _MODULE_PATH / 'water_TIP3P.sdf'
 if not _water_path.exists():
     water = generate_water_TIP3P()
     topology.package_atom_metadata(water, in_place=True)
-    topology.save_molecule(_water_path, water, toolkit_registry=TKREGS['OpenEye Toolkit'])
+    topology.save_molecule(_water_path, water)
 
 # register Molcules for all registered solvents
 for path in _MODULE_PATH.iterdir():
