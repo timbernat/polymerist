7f2478ff56ee2ff3ee36e14cb5fc7df620864983
Timotej Bernat <tibe3324@colorado.edu>
2024-03-08 21:13:02 -0700

    Added support for drawing without replacement and bin uniquification to bin_ids_forming_sequence

--- genutils/sequences.py
+++ genutils/sequences.py
@@ -1,16 +1,14 @@
 '''Tools for generating and manipulating ordered sequences of numbers'''
 
 from typing import Generator, Iterable, Sequence, TypeVar, Union
-
-from operator import mul
-from collections import defaultdict, Counter
-
-from functools import reduce
-from itertools import count, product as cartesian_product
-
-
 T = TypeVar('T') # generic type for sequence element
 U = TypeVar('U') # generic type for a distinct sequence element
+
+from copy import deepcopy
+from operator import mul
+from collections import defaultdict, Counter
+from functools import reduce
+from itertools import count, product as cartesian_product
 
 
 def is_unique(seq : Sequence) -> bool:
@@ -53,16 +51,34 @@ def cycle_items(seq : Sequence[T], places : int=1) -> list[T]:
             for i in range(places, places + n_items)
     ]
 
-def bin_ids_forming_sequence(sequence : Iterable[T], choice_bins : Iterable[Iterable[T]], unique_bins : bool=False) -> Generator[tuple[int, ...], None, None]:
-    '''Takes an ordered sequence of N objects and a collection of any number of bins containing arbitrary objects and generates
-    all possible N-tuples of bin indices which could produce the target sequence when drawing from those bins WITH replacement
+def bin_ids_forming_sequence(sequence : Sequence[T], choice_bins : Sequence[Iterable[T]], draw_without_repeats : bool=True, unique_bins : bool=False) -> Generator[tuple[int, ...], None, None]:
+    '''Takes an ordered sequence of N objects of a given type and an ordered of any number of bins, each containing an arbitary amount of unordered objects of the same type
+    Generates all possible N-tuples of bin indices which could produce the target sequence when drawing from those bins in the 
     
-    If unique_bins is True, will not return a sequence in which a bin can appear more than once''' 
-    occurrences = defaultdict(Counter) # keys are objects, values give indices of bins in which the objects occur, along with multiplicities 
-    for i, tags in enumerate(choice_bins):
-        for tag in tags:
-            occurrences[tag][i] += 1 # NOTE : implementation here requires that T be a hashable type
+    if draw_without_repeats=True, will respect the multiplicity of elements in each bin when drawing
+    (i.e. will never have a bin position appear for a given object more times that that object appears in the corresponding bin)
 
-    for idxs in cartesian_product(*(occurrences[item].keys() for item in sequence)): # TODO : enable no-replacement scheme 
-        if (not unique_bins) or is_unique(idxs):
-            yield idxs
+    if unique_bins=True, will only allow each bin to be sampled from once, EVEN if that bin contains elements which may occur later in the sequence'''
+    symbol_inventory = defaultdict(Counter) # keys are objects of type T ("symbols"), values give multiplicities of symbols keyed by bin position
+    for i, choice_bin in enumerate(choice_bins):
+        for sym in choice_bin:
+            symbol_inventory[sym][i] += 1 # NOTE : implementation here requires that T be a hashable type
+
+    for idxs in cartesian_product(*(symbol_inventory[item].keys() for item in sequence)): # generate every valid sequence of bin positions WITHOUT regard to repetition or uniqueness
+        if unique_bins and not is_unique(idxs):
+            continue # skip non-unique bin choices if the option is set
+
+        if draw_without_repeats:
+            choice_inventory = deepcopy(symbol_inventory) # make a new copy for each path check
+            for i, sym in zip(idxs, sequence, strict=True):
+                if choice_inventory[sym][i] == 0:
+                    overdrawn = True
+                    break
+                choice_inventory[sym][i] -= 1
+            else:
+                overdrawn = False
+
+            if overdrawn:
+                continue
+        
+        yield idxs # only yield if all specified conditions are met
