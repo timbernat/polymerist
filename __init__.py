cdf2d5e921d47385c228eb25aad13a17250cf1c5
Timotej Bernat <tibe3324@colorado.edu>
2024-03-14 17:56:02 -0600

    Initial commit with MolSSI Cookiecutter template

--- __init__.py
+++
@@ -1,9 +0,0 @@
-'''Polymer-Oriented Library Yielding Structure Assignment, Calculation of CHARges, Interchange, and Data Elucidation (second revision)'''
-
-__author__ = 'Timotej Bernat'
-__version__ = '2.0'
-
-from .genutils.importutils import register_submodules, module_by_pkg_str
-
-_MODULE_SELF = module_by_pkg_str(__package__) # keep reference to own module
-register_submodules(_MODULE_SELF, recursive=True, blacklist=['analysis'])
