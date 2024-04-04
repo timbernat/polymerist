d2b9de078d716c9686f645f5e832938d8853da43
Timotej Bernat <tibe3324@colorado.edu>
2024-04-03 20:05:11 -0600

    Added module-wide references to ASCII character subsets

--- polymerist/genutils/textual/__init__.py
+++ polymerist/genutils/textual/__init__.py
@@ -1,1 +1,4 @@
 '''Tools for manipulating, processing, and pretty-printing text from files and string-like objects'''
+
+from string import ascii_letters, ascii_lowercase, ascii_uppercase
+ascii_printable = ''.join(chr(i) for i in range(33, 127)) # ASCII printable characters, minus SPACE (" ", 32) and DELETE (127)
