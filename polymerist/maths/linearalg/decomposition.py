712cca62faee8703bdd672548d9716213d58421b
Timotej Bernat <tibe3324@colorado.edu>
2024-04-19 12:55:47 -0600

    Added left and right matrix pseudo-inverse calculations

--- polymerist/maths/linearalg/decomposition.py
+++ polymerist/maths/linearalg/decomposition.py
@@ -1,6 +1,8 @@
-'''Tools for matric decomposition'''
+'''Tools for matrix decomposition'''
 
 import numpy as np
+from ...genutils.typetools import Shape, DType, N, M
+
 
 def diagonalize(matrix : np.ndarray) -> tuple[np.ndarray]:
     '''Diagonalize a matrix into it's eigenbasis. Return rotation and diagonal matrices P, D, and P^-1''' 
@@ -10,3 +12,11 @@ def diagonalize(matrix : np.ndarray) -> tuple[np.ndarray]:
     Pinv = np.linalg.inv(eivecs) # compute inverse matrix to rotate back
 
     return P, D, Pinv 
+
+def inv_left(matrix : np.ndarray[Shape[M, N], DType]) -> np.ndarray[Shape[N, M], DType]:
+    '''Return the left-inverse of an arbitrary (not necessarily square) matrix'''
+    return np.linalg.inv(matrix.T @ matrix) @ matrix.T
+
+def inv_right(matrix : np.ndarray[Shape[M, N], DType]) -> np.ndarray[Shape[N, M], DType]:
+    '''Return the right-inverse of an arbitrary (not necessarily square) matrix'''
+    return matrix.T @ np.linalg.inv(matrix @ matrix.T)
