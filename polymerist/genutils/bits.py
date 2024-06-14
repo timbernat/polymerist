344a34a5f4941e949c82d3e4a0a341943890a6c8
Timotej Bernat <tibe3324@colorado.edu>
2024-06-14 16:11:02 -0600

    Added new genutils.bits package for bitwise operations

---
+++ polymerist/genutils/bits.py
@@ -0,0 +1,18 @@
+'''For bitwise operations and conversions to/from bitstrings'''
+
+from typing import Union 
+
+
+def int_to_bits(n : int, num_bits : int=8, clamp : bool=False, as_list : bool=False) -> Union[str, list[int]]:
+    '''
+    Convert an integer into a string of its bits, padded out to <num_bits> bits
+    If clamp=True and the binary representation of <n> has more than <num_bits> bits, then leading bits will be discarded
+    If as_list=True, will return as list of 0/1 ints; otherwise, will return as string
+    '''
+    bitstring = f'{n:0{num_bits}b}' # format into given number of bits with leading zeros - way easier than directly implementing bitshift-based operations
+    if clamp:
+        bitstring = bitstring[-num_bits:]
+
+    if as_list:
+        return [int(bit) for bit in bitstring] 
+    return bitstring
