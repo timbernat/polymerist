cbba4c3973a1ba169cf66f86129a371a23ebc661
Timotej Bernat <tibe3324@colorado.edu>
2024-08-15 13:24:27 -0600

    Created new genutils.attrs submodule for inspecting attributes

---
+++ polymerist/genutils/attrs.py
@@ -0,0 +1,18 @@
+'''For dynamically inspecting and modifying attributes of Python objects'''
+
+from typing import Any, Optional
+import re
+
+
+def compile_simple_getable_attrs(obj : Any, getter_str : str='get', repl_str : Optional[str]=None) -> dict[str, Any]:
+    '''Takes an object and returns a dict of the return values of all argument-free methods of the objects
+    Looks for methods of the object whose names contain with "getter_str", and can replace this with the value of "repl_str" in the final dict output if provided'''
+    getable_dict = {}
+    for attr_name in dir(obj):
+        if re.search(getter_str, attr_name): 
+            try:
+                attr_key = attr_name if (repl_str is None) else re.sub(getter_str, repl_str, attr_name)
+                getable_dict[attr_key] = getattr(obj, attr_name)()
+            except (TypeError, Exception): # TODO : find way to selectively intercept the Boost C++ wrapper ArgumentError
+                pass
+    return getable_dict
