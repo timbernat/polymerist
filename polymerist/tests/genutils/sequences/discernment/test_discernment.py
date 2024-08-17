61e34e0dc271c93903643105e35e86ffbf4a9b2f
Timotej Bernat <tibe3324@colorado.edu>
2024-08-16 20:18:43 -0600

    Moved discernment unit tests to tests submodule, set up basic pytest fixtures for correct solution read

--- polymerist/tests/genutils/sequences/discernment/test_discernment.py
+++ polymerist/tests/genutils/sequences/discernment/test_discernment.py
@@ -1,33 +1,43 @@
 '''Unit tests for DISCERNMENT-related functionality'''
 
-# TODO: add logging
+import pytest
+from polymerist.genutils.pkginspect import get_file_path_within_package
+from polymerist.tests import data as testdata
 
 import json
 from pathlib import Path
 
-from .inventory import SymbolInventory
-from .strategies import DISCERNMENTStrategy
+from polymerist.genutils.sequences.discernment.inventory import SymbolInventory
+from polymerist.genutils.sequences.discernment.strategies import DISCERNMENTStrategy
 
 
-class DISCERNMENTIncosistencyError(Exception):
+class DISCERNMENTInconsistencyError(Exception):
     '''Custom Exception for indicating inconsistencies between DISCERNMENT Strategy implementations'''
     pass
 
-def check_discernment_algorithm_consistency(ignore_multiplicities : bool=False, unique_bins : bool=False) -> None:
+
+@pytest.fixture(scope='module')
+def solution_path():
+    return get_file_path_within_package('correct_discernment_solution.json', testdata)
+
+@pytest.fixture(scope='module')
+def correct_solution(solution_path):
+    with solution_path.open('r') as file:
+        solution = set(
+            tuple(indices)
+                for indices in json.load(file)
+        )
+    return solution
+
+
+def test_discernment_algorithm_consistency(correct_solution : set[tuple[int, ...]], ignore_multiplicities : bool=False, unique_bins : bool=False) -> None:
     '''Check to ensure that all implementations of DISCERNMENT solution strageties yield the same outputs and don't modify a provided symbol inventory
     Raises failure-specific Exception if inconsistency is detected, terminates silently (no Exception, returns None) otherwise'''
     # hard-code inputs and expected solution
     WORD = 'accg'
     CHOICE_BINS = ('bbc','aced','bad','daea','fccce','g','abcd','fggegc')
 
-    LOCAL_DIR = Path(__file__).parent
-    SOLUTION_PATH = LOCAL_DIR / 'correct_discernment_solution.json'
-
-    with SOLUTION_PATH.open('r') as file:
-        CORRECT_SOLUTION = set(
-            tuple(indices)
-                for indices in json.load(file)
-        )
+    print(correct_solution)
 
     sym_inv = SymbolInventory.from_bins(CHOICE_BINS)
     orig_sym_inv = sym_inv.deepcopy()
@@ -46,14 +56,14 @@ def check_discernment_algorithm_consistency(ignore_multiplicities : bool=False, 
                     unique_bins=unique_bins
                 )
         )
-        if solution != CORRECT_SOLUTION: # check that answer produces is accurate
-            raise DISCERNMENTIncosistencyError(f'Algorithm {method_name} does not produce to correct enumeration of bin labels') 
+        if solution != correct_solution: # check that answer produces is accurate
+            raise DISCERNMENTInconsistencyError(f'Algorithm {method_name} does not produce to correct enumeration of bin labels') 
         if sym_inv != orig_sym_inv: # check that the symbol inventory is unmodified
-            raise DISCERNMENTIncosistencyError(f'Algorithm {method_name} does not return symbol inventory to original state after completion')
+            raise DISCERNMENTInconsistencyError(f'Algorithm {method_name} does not return symbol inventory to original state after completion')
         for other_method_name, other_solution in all_results.items(): # check against all other methods PRIOR TO INSERTION (minimal number of checks guaranteed to verify all pairwise checks)
             # print(method_name, other_method_name)
             if (solution - other_solution != set()) or (other_solution - solution != set()): # check both symmetric differences to make sure none are 
-                raise DISCERNMENTIncosistencyError(f'Algorithms {method_name} and {other_method_name} produce inconsistent solutions')
+                raise DISCERNMENTInconsistencyError(f'Algorithms {method_name} and {other_method_name} produce inconsistent solutions')
 
         # implicit else
         all_results[method_name] = solution
