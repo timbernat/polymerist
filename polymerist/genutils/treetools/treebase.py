cb2681419083c8a944ccc1ec5c8076f137a90574
Timotej Bernat <tibe3324@colorado.edu>
2024-11-13 15:14:07 -0700

    Expanded docstrings, added exclude filter mixin and check for Node attribute overrides

--- polymerist/genutils/treetools/treebase.py
+++ polymerist/genutils/treetools/treebase.py
@@ -34,16 +34,50 @@ def compile_tree_factory(
         node_corresp : NodeCorrespondence[T],
         class_alias : Optional[str]=None,
         obj_attr_name : Optional[str]=None,
-    ) -> Callable[[T, Optional[int]], Node]:
-    '''Factory method for producing a tree-generating function for the given Type''' # TODO: include blacklist
+        exclude_mixin : Optional[Filter[T]]=None,
+    ) -> Callable[[T, Optional[int], Optional[Filter[T]]], Node]:
+    '''
+    Factory method for producing a tree-generating function from a NodeCorrespondence
+    
+    Parameters
+    ----------
+    node_corresp : NodeCorrespondence[T]
+        Definition of a correpondence between an arbitrary type and a Tree Node
+    class_alias : str (optional)
+        Name of the corresponding class to inject into docstring
+        If not provided, will default to the __name__ of the class wrapped by node_corresp
+    obj_attr_name : str (optional)
+        The name of the Node attribute to which a copy of the class instance should be bound
+        If not provided, will default to the value of class_alias
+    exclude_mixin : Filter[T] (optional)
+        An optional "master" filter to mix into any 
+
+    Returns
+    -------
+    compile_tree : Callable[[T, Optional[int], Optional[Filter[T]]], Node]
+        Factory function which takes an instance of type T, builds a Tree from it, and returns the root Node
+    '''
+    node_type_name = node_corresp.FROMTYPE.__name__
     if class_alias is None: # an alternative name to use when describing the tree creation for this class
-        class_alias = node_corresp.FROMTYPE.__name__
+        class_alias = node_type_name
 
     if obj_attr_name is None: # the name given to the Node attribute which store an instance of the given arbitrary type
         obj_attr_name = class_alias
+    if hasattr(Node, obj_attr_name):
+        raise AttributeError(f'Invalid value for obj_attr_name; attribute "{obj_attr_name}" clashes with existing attribute Node.{obj_attr_name}')
 
-    def compile_tree(obj : node_corresp.FROMTYPE,  max_depth : Optional[int]=None, exclude : Filter[node_corresp.FROMTYPE]=ALWAYS_FALSE_FILTER, _curr_depth : int=0) -> Node:
+    if exclude_mixin is None:
+        exclude_mixin = ALWAYS_FALSE_FILTER
+
+    def compile_tree(
+        obj : node_corresp.FROMTYPE,
+        max_depth : Optional[int]=None,
+        exclude : Filter[node_corresp.FROMTYPE]=ALWAYS_FALSE_FILTER,
+        _curr_depth : int=0
+    ) -> Node:
         # NOTE: deliberately omitting docstring here, as it will be built procedurally after defining this function
+        exclude = lambda obj : exclude_mixin(obj) | exclude(obj) # incorporate mixin into exclusion criterion # TODO: make this more efficient for double-defaults
+
         node = Node(name=node_corresp.name(obj))
         setattr(node, obj_attr_name, obj) # keep an instance of the object directly for reference
 
@@ -60,10 +94,22 @@ def compile_tree_factory(
 
     # annoyingly, docstrings must be string literals (this CAN'T be done inside the function definition)
     compile_tree.__doc__ = f''' 
-    Compile a {class_alias} tree from a(n) {node_corresp.FROMTYPE.__name__} object
+    Compile a {class_alias} tree from a(n) {node_type_name} object
 
     Any sub-{class_alias} encountered will be expanded into its own tree,
     up to the specified maximum depth, or until exhaustion if max_depth=None
+
+    Parameters
+    ----------
+    obj : {node_type_name}
+        A(n) instance of a {class_alias}
+    max_depth : int (optional)
+        Maximum allowed height of a constructed tree from the root
+        If None (as default), no limit is set
+    exclude : Filter[{node_type_name}] (optional)
+        An optional filter function to reduce the size of the constructed tree
+        Must accept a(n) {node_type_name} instance as single argument
+        Should return True when a node is to be excluded, and False otherwise
     '''
 
     return compile_tree
