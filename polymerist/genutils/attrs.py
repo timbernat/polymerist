5258458e9b04312059b187a11a19c7d9c0f486b0
Timotej Bernat <tibe3324@colorado.edu>
2024-11-13 13:51:44 -0700

    Wrote unit tests for genutils.attrs

--- polymerist/genutils/attrs.py
+++ polymerist/genutils/attrs.py
@@ -5,7 +5,7 @@ import re
 
 
 def compile_argfree_getable_attrs(obj : Any, getter_re : Union[str, re.Pattern]='.*', repl_str : Optional[str]=None) -> dict[str, Any]:
-    '''Compile the values of all methods of an object which require no arguments other than perhaps the object itself (this includes properties)
+    '''Compile the values of all methods of an object which require no arguments other than perhaps the object itself (this EXCLUDES properties)
     Returns a dict whose keys are the names of the methods called and whose values are the return values of those object methods
 
     Can optionally filter the names of returned method using a regular expression, passed to "getter_re"
