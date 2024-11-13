64dd928e4dda8ae38327cc9a83a9630765848ea4
Timotej Bernat <tibe3324@colorado.edu>
2024-11-13 13:12:05 -0700

    Expanded docstring to conform to numpy docstring standard

--- polymerist/genutils/attrs.py
+++ polymerist/genutils/attrs.py
@@ -1,12 +1,32 @@
 '''For dynamically inspecting and modifying attributes of Python objects'''
 
-from typing import Any, Optional
+from typing import Any, Optional, Union
 import re
 
 
-def compile_argfree_getable_attrs(obj : Any, getter_re : str='get', repl_str : Optional[str]=None) -> dict[str, Any]:
-    '''Takes an object and returns a dict of the return values of all argument-free methods of the objects
-    Looks for methods of the object whose names contain with "getter_re", and can replace this with the value of "repl_str" in the final dict output if provided'''
+def compile_argfree_getable_attrs(obj : Any, getter_re : Union[str, re.Pattern]='.*', repl_str : Optional[str]=None) -> dict[str, Any]:
+    '''Compile the values of all methods of an object which require no arguments other than perhaps the object itself (this includes properties)
+    Returns a dict whose keys are the names of the methods called and whose values are the return values of those object methods
+
+    Can optionally filter the names of returned method using a regular expression, passed to "getter_re"
+    Can also optionally replace the chosen regex with an arbitrary string (including the empty string), passed to "repl_str"
+    
+    Parameters
+    ----------
+    obj : Any
+        Any object instance
+    getter_re : str or re.Pattern (optional), default ".*"
+        Optional regular expression to use for filtering down returned methods
+        Only methods whose names match the target regex are returns
+    repl_str : str (optional)
+        If provided, will replace the 
+        for example, repl_str="" can be used to delete the regex from returned method names
+
+    Returns
+    -------
+    getable_dict : dict[str, Any]
+        dict whose keys are the selected method names and whose values are the corresponding method returns
+    '''
     getable_dict = {}
     for attr_name in dir(obj):
         if re.search(getter_re, attr_name): 
