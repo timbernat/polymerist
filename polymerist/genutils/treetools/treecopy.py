1b220e53d7c633d07f286a4e5b899c3ef86157e1
Timotej Bernat <tibe3324@colorado.edu>
2024-11-18 14:40:31 -0700

    Implemented anytree-to-networkx converter

--- polymerist/genutils/treetools/treecopy.py
+++ polymerist/genutils/treetools/treecopy.py
@@ -2,9 +2,11 @@
 
 from typing import Any, Optional
 
-from functools import reduce
 from anytree.node import Node
+from anytree.iterators import PreOrderIter
 from anytree.exporter import DictExporter
+
+from networkx import DiGraph
 
 from ..filters import Filter, ALWAYS_TRUE_FILTER, ALWAYS_FALSE_FILTER
 
@@ -20,6 +22,7 @@ def get_node_attrs(node : Node, attr_filter : Optional[Filter[str]]=None, includ
     attr_filter : Filter[str] (optional), default lambda x : True
         An optional criterion to decide whether an attribute should be kept
         Should be a function which accepts a single string arg and returns a bool
+        
         Return value of True will include an attribute in the return, while value or False will exclude it
         If None, will default to always True (i.e. no attributes will be screened out)
     include_name : bool, deafult False
@@ -59,6 +62,7 @@ def copy_node_unbound(node : Node, attr_filter : Optional[Filter[str]]=None) -> 
     attr_filter : Filter[str] (optional), default lambda x : True
         An optional criterion to decide whether an attribute should be kept
         Should be a function which accepts a single string arg and returns a bool
+
         Return value of True will include an attribute in the return, while value or False will exclude it
         If None, will default to always True (i.e. no attributes will be screened out)
 
@@ -75,7 +79,7 @@ def copy_tree(root : Node, stop : Optional[Filter[Node]]=None, attr_filter : Opt
     '''
     Create a copy of an anytree Node hierarchy. Can provide filters and stop criteria to exclude nodes or whole branches
     
-        Parameters
+    Parameters
     ----------
     root : Node
         An anytree.node.Node object which is the root of a tree-like hierarchy
@@ -91,6 +95,7 @@ def copy_tree(root : Node, stop : Optional[Filter[Node]]=None, attr_filter : Opt
     attr_filter : Filter[str] (optional), default lambda x : True
         An optional criterion to decide whether an attribute should be kept
         Should be a function which accepts a single string arg and returns a bool
+
         Return value of True will include an attribute in the return, while value or False will exclude it
         If None, will default to always True (i.e. no attributes will be screened out)
 
@@ -110,3 +115,58 @@ def copy_tree(root : Node, stop : Optional[Filter[Node]]=None, attr_filter : Opt
         child_copy.parent = root_new
 
     return root_new
+
+def tree_to_networkx(root : Node, stop : Optional[Filter[Node]]=None, attr_filter : Optional[Filter[str]]=None) -> Node: 
+    '''
+    Produces a networkx.DiGraph representation on an anytree Tree
+
+    Parameters
+    ----------
+    root : Node
+        An anytree.node.Node object which is the root of a tree-like hierarchy
+    stop : Filter[Node] (optional), default None
+        An optional criterion to decide when to stop traversing the tree to be copied
+        Should be a function which accepts a single Node arg and returns a bool
+        
+        Return value of True will exclude all subsequent nodes on a branch, while value of True will proceed with iteration and copying 
+        If None, will default to always False (i.e. no extra stop conditions, full tree will be copied)
+
+        This criterion is inclusive, i.e. a Node matching this criterion will NOT be 
+        included in the copied tree, nor will any of its children or their children, recursively
+    attr_filter : Filter[str] (optional), default lambda x : True
+        An optional criterion to decide whether an attribute should be kept
+        Should be a function which accepts a single string arg and returns a bool
+
+        Return value of True will include an attribute in the return, while value or False will exclude it
+        If None, will default to always True (i.e. no attributes will be screened out)
+
+    Returns
+    -------
+    nx_tree : diGraph
+        A networkx directed graph object 
+    '''
+    if stop is None:
+        stop = ALWAYS_FALSE_FILTER
+
+    pruned : dict[Node, bool] = {}  # which nodes should be excluded (needed for recursive branch pruning)
+    node_ids : dict[Node, int] = {} # unique integer index for nodes (needed to resolve duplicate-name Nodes)
+    nx_tree = DiGraph()
+    for i, node in enumerate(PreOrderIter(root)): # NOTE: DON'T CHANGE ITERATOR!, implementation here relies on the topological sorting property of pre-order traversal
+        node_ids[node] = i # mark node for parent identification later
+        if stop(node):
+            pruned[node] = True
+            continue # skip over node addition once marked
+
+        parent_idx : Optional[int] = None
+        if node.parent is not None:
+            parent_idx = node_ids[node.parent]
+            if pruned[node.parent]: # if the parent node was pruned...
+                pruned[node] = True # ...mark the current node as also pruned...
+                continue            # ...and skip over its addition
+        
+        nx_tree.add_node(i, label=node.name, **get_node_attrs(node, attr_filter=attr_filter, include_name=False))
+        if parent_idx is not None:
+            nx_tree.add_edge(parent_idx, i) # parent guaranteed to have been visited first and be mapped by pre-order topological sorting
+        pruned[node] = False # only once a node is added to the graph should it be explicitly marked as not pruned
+
+    return nx_tree
