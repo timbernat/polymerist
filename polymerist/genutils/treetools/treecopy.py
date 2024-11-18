fa461f8073c7ba10ce4c89c32d89d5337b5063f9
Timotej Bernat <tibe3324@colorado.edu>
2024-11-18 13:09:23 -0700

    Separated node attr extraction from copying, enhanced docstrings

--- polymerist/genutils/treetools/treecopy.py
+++ polymerist/genutils/treetools/treecopy.py
@@ -1,6 +1,6 @@
 '''Tools for copying parts and wholes of trees, at various levels of resolution'''
 
-from typing import Optional
+from typing import Any, Optional
 
 from anytree.node import Node
 from anytree.exporter import DictExporter
@@ -8,24 +8,91 @@ from anytree.exporter import DictExporter
 from ..filters import Filter, ALWAYS_TRUE_FILTER, ALWAYS_FALSE_FILTER
 
 
-def copy_node_attrs(node : Node, attr_filter : Filter[str]=ALWAYS_TRUE_FILTER) -> Node:
-    '''Create a copy of a Node with only its attributes and no set ancestors or descendents'''
+def get_node_attrs(node : Node, attr_filter : Optional[Filter[str]]=None) -> dict[str, Any]:
+    '''
+    Return a dict of all attributes set on a Node
+    
+    Parameters
+    ----------
+    node : Node
+        An anytree.node.Node object
+    attr_filter : Filter[str] (optional), default lambda x : True
+        An optional criterion to decide whether an attribute should be kept
+        Should be a function which accepts a single string arg and returns a bool
+        Return value of True will include an attribute in the return, while value or False will exclude it
+        If None, will default to always True (i.e. no attributes will be screened out)
+
+    Returns
+    -------
+    node_attrs : dict[str, Any]
+        A dictionary keyed by attribute name whose values are the values set for repective Node attributes
+    '''
+    if attr_filter is None:
+        attr_filter = ALWAYS_TRUE_FILTER
+
     node_attrs = {
         attr_name : attr
             for attr_name, attr in DictExporter._iter_attr_values(node) # NOTE: this is necessary to omit mangled NodeMixin info about parents and children
                 if attr_filter(attr_name)
     }
     # assert(node_copy.children == tuple())
-    return Node(**node_attrs)
+    return node_attrs
+
+def copy_node_unbound(node : Node, attr_filter : Optional[Filter[str]]=None) -> Node:
+    '''
+    Create a copy of a Node with matching attributes but no ancestors or descendents
+
+    Parameters
+    ----------
+    node : Node
+        An anytree.node.Node object
+    attr_filter : Filter[str] (optional), default lambda x : True
+        An optional criterion to decide whether an attribute should be kept
+        Should be a function which accepts a single string arg and returns a bool
+        Return value of True will include an attribute in the return, while value or False will exclude it
+        If None, will default to always True (i.e. no attributes will be screened out)
+
+    Returns
+    -------
+    node_copy : Node
+        A new Node object which has the same attributes as
+        the original node but none of the ancestors or descendants
+    '''
+    return Node(**get_node_attrs(node=node, attr_filter=attr_filter))
 
 # NOTE: explicitly exclude a filter criterion here, as filtering (rather than stopping) may result in deleted nodes IN THE MIDDLE of a tree
-def copy_tree(node : Node, stop : Filter[Node]=ALWAYS_FALSE_FILTER, attr_filter : Filter[str]=ALWAYS_TRUE_FILTER) -> Node: 
-    '''Create a copy of an anytree Node hierarchy. Can provide filters and stop criteria to exclude nodes or whole branches'''
-    node_copy = copy_node_attrs(node, attr_filter=attr_filter) # make a read-only copy of JUST the current node's attributes
-    for child in node.children: # NOTE: this also works for leaf nodes, as their "children" attrs is just an empty tuple
+def copy_tree(root : Node, stop : Optional[Filter[Node]]=None, attr_filter : Optional[Filter[str]]=None) -> Node: 
+    '''
+    Create a copy of an anytree Node hierarchy. Can provide filters and stop criteria to exclude nodes or whole branches
+    
+        Parameters
+    ----------
+    root : Node
+        An anytree.node.Node object which is the root of a tree-like hierarchy
+    stop : Filter[Node] (optional), default None
+        An optional criterion to decide when to stop traversing the tree to be copied
+        Should be a function which accepts a single Node arg and returns a bool
+        Return value of True will exclude all subsequent nodes on a branch, while value of True will proceed with iteration and copying 
+        If None, will default to always False (i.e. no extra stop conditions, full tree will be copied)
+    attr_filter : Filter[str] (optional), default lambda x : True
+        An optional criterion to decide whether an attribute should be kept
+        Should be a function which accepts a single string arg and returns a bool
+        Return value of True will include an attribute in the return, while value or False will exclude it
+        If None, will default to always True (i.e. no attributes will be screened out)
+
+    Returns
+    -------
+    root_new : Node
+        The root Node of the copied tree structure
+    '''
+    if stop is None:
+        stop = ALWAYS_FALSE_FILTER
+
+    root_new = copy_node_unbound(root, attr_filter=attr_filter)
+    for child in root.children: # NOTE: this also works for leaf nodes, as their "children" attrs is just an empty tuple
         if stop(child):
             continue
         child_copy = copy_tree(child, stop=stop, attr_filter=attr_filter) # recursively copy children until stop criterion
-        child_copy.parent = node_copy
+        child_copy.parent = root_new
 
-    return node_copy
+    return root_new
