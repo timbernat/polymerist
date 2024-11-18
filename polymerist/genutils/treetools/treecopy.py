41d1fea68ad6908b25a7bdab58f25f6d0e7f975d
Timotej Bernat <tibe3324@colorado.edu>
2024-11-18 13:33:51 -0700

    Added option to omit "name" from get_node_attributes (useful for copying)

--- polymerist/genutils/treetools/treecopy.py
+++ polymerist/genutils/treetools/treecopy.py
@@ -2,13 +2,14 @@
 
 from typing import Any, Optional
 
+from functools import reduce
 from anytree.node import Node
 from anytree.exporter import DictExporter
 
 from ..filters import Filter, ALWAYS_TRUE_FILTER, ALWAYS_FALSE_FILTER
 
 
-def get_node_attrs(node : Node, attr_filter : Optional[Filter[str]]=None) -> dict[str, Any]:
+def get_node_attrs(node : Node, attr_filter : Optional[Filter[str]]=None, include_name : bool=False) -> dict[str, Any]:
     '''
     Return a dict of all attributes set on a Node
     
@@ -21,6 +22,10 @@ def get_node_attrs(node : Node, attr_filter : Optional[Filter[str]]=None) -> dic
         Should be a function which accepts a single string arg and returns a bool
         Return value of True will include an attribute in the return, while value or False will exclude it
         If None, will default to always True (i.e. no attributes will be screened out)
+    include_name : bool, deafult False
+        Whether to include the required "name" attribute of a Node in the returned dict
+        Useful to exclude when copying nodes to avoid redundancy
+        By default False (i.e. "name" will be excluded from the returned attributes)
 
     Returns
     -------
@@ -30,10 +35,15 @@ def get_node_attrs(node : Node, attr_filter : Optional[Filter[str]]=None) -> dic
     if attr_filter is None:
         attr_filter = ALWAYS_TRUE_FILTER
 
+    if not include_name: # NOTE: need to rebind to different name to avoid recursion error
+        _attr_filter = lambda attr_name : attr_filter(attr_name) and attr_name != 'name'
+    else:
+        _attr_filter = attr_filter
+
     node_attrs = {
         attr_name : attr
             for attr_name, attr in DictExporter._iter_attr_values(node) # NOTE: this is necessary to omit mangled NodeMixin info about parents and children
-                if attr_filter(attr_name)
+                if _attr_filter(attr_name)
     }
     # assert(node_copy.children == tuple())
     return node_attrs
@@ -58,7 +68,7 @@ def copy_node_unbound(node : Node, attr_filter : Optional[Filter[str]]=None) -> 
         A new Node object which has the same attributes as
         the original node but none of the ancestors or descendants
     '''
-    return Node(**get_node_attrs(node=node, attr_filter=attr_filter))
+    return Node(name=node.name, **get_node_attrs(node=node, attr_filter=attr_filter, include_name=False))
 
 # NOTE: explicitly exclude a filter criterion here, as filtering (rather than stopping) may result in deleted nodes IN THE MIDDLE of a tree
 def copy_tree(root : Node, stop : Optional[Filter[Node]]=None, attr_filter : Optional[Filter[str]]=None) -> Node: 
