aee97bcf2428285d6103b05ac29db15cf3888050
Timotej Bernat <tibe3324@colorado.edu>
2024-11-18 17:09:58 -0700

    Wrote unit test for anytree-to-networkx conversion

--- polymerist/tests/genutils/trees/test_trees.py
+++ polymerist/tests/genutils/trees/test_trees.py
@@ -7,7 +7,9 @@ from anytree.node import Node
 from anytree.search import find as find_node
 from anytree.iterators import AbstractIter, PreOrderIter, PostOrderIter, LevelOrderIter
 
-from polymerist.genutils.trees.treecopy import get_node_attrs, copy_node_unbound, copy_tree
+from networkx import dfs_preorder_nodes
+
+from polymerist.genutils.trees.treecopy import get_node_attrs, copy_node_unbound, copy_tree, tree_to_networkx
 
 
 @pytest.fixture
@@ -15,11 +17,11 @@ def example_tree() -> Node:
     '''Produce a simplified tree for performing tests'''
     root = Node('f')
     b = Node('b', parent=root, foo='bb')
-    a = Node('a', parent=b,    foo='aa', bar='bar')
+    a = Node('a', parent=b,    foo='aa', bar='spam')
     d = Node('d', parent=b,    foo='dd')
-    c = Node('c', parent=d,    foo='cc', bar='bar')
+    c = Node('c', parent=d,    foo='cc', bar='spam')
     e = Node('e', parent=d,    foo='ee', baz=[1,2,3])
-    g = Node('g', parent=root, foo='gg', bar='bar', baz=[1,2,3])
+    g = Node('g', parent=root, foo='gg', bar='spam', baz=[1,2,3])
     i = Node('i', parent=g,    foo='ii')
     h = Node('h', parent=i,    foo='hh')
     a_dup = Node('a', foo='a+a', parent=g) # testing how nodes with duplicate names are handled
@@ -105,4 +107,13 @@ def test_copy_tree_stop(example_tree : Node) -> None:
     assert all(
         copied_node.name != 'a'
             for copied_node in PreOrderIter(copied_tree) # in this case, the iteration order uniquely doesn't matter, only care that all nodes are traversed 
+    )
+
+
+def test_tree_to_networkx(example_tree : Node) -> None:
+    '''Test that conversion to networkx.DiGraph faithfully reproduces node order and attributes'''
+    nxtree = tree_to_networkx(example_tree)
+    assert all(
+        nxtree.nodes[i] == get_node_attrs(at_node, include_name=True)
+            for i, at_node in zip(dfs_preorder_nodes(nxtree), PreOrderIter(example_tree)) # preorder should match, regardless of implementation
     )
