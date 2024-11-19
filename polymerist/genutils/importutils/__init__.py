2ba0996b020591545bdaccaa5522bda48f920dbc
Timotej Bernat <tibe3324@colorado.edu>
2024-11-18 17:56:05 -0700

    Exposed submodule classes and functions at the subpackage levels

--- polymerist/genutils/importutils/__init__.py
+++ polymerist/genutils/importutils/__init__.py
@@ -1,4 +1,4 @@
 '''Functionality for dynamically importing and inspecting Python modules and packages'''
 
-from .pkgiter import module_hierarchy, iter_submodules
+from .pkgiter import module_hierarchy, iter_submodules, module_tree, module_tree_direct
 from .pkginspect import is_package, is_module
--- polymerist/genutils/trees/__init__.py
+++ polymerist/genutils/trees/__init__.py
@@ -1,5 +1,5 @@
 '''Generic functionality for tree-like data structures. Based on the anytree module (https://github.com/c0fec0de/anytree)'''
 
 from .treebase import NodeCorrespondence, compile_tree_factory
-from .treecopy import copy_tree
+from .treecopy import get_node_attrs, copy_tree, tree_to_networkx
 from .treeviz import treestr
