6ec9f06d62f594ebe111458ed98ff87143c1f64f
Timotej Bernat <tibe3324@colorado.edu>
2024-11-18 18:04:37 -0700

    Baked importutils directly into toplevel module

--- polymerist/__init__.py
+++ polymerist/__init__.py
@@ -2,6 +2,7 @@
 
 # Add imports here
 from ._version import __version__
+from .genutils import importutils
 # from .genutils.importutils import register_submodules, module_by_pkg_str
 
 # _MODULE_SELF = module_by_pkg_str(__package__) # keep reference to own module
