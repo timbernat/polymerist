e14afd73c714bf602cc48d76eccd4cd6fc05a3ef
Timotej Bernat <tibe3324@colorado.edu>
2024-11-20 18:09:52 -0700

    Added unit tests module for mdutils.openfftools.partialcharge.molchargers

---
+++ polymerist/tests/mdtools/openfftools/partialcharge/test_molchargers.py
@@ -0,0 +1,13 @@
+'''Unit tests for `molchargers` package'''
+
+__author__ = 'Timotej Bernat'
+__email__ = 'timotej.bernat@colorado.edu'
+
+import pytest
+
+from polymerist.mdtools.openfftools.partialcharge import molchargers
+
+
+def test_CHARGING_METHOD_class_attr() -> None:
+    '''Test that the MolCharger abstract class attribute "CHARGING_METHOD" is properly registere and enforced'''
+    pass
