e102159fa5fbe28c88b6f8aa4deb94ff8717e26b
Timotej Bernat <tibe3324@colorado.edu>
2024-11-20 21:01:43 -0700

    Added OpenFF Toolkit-specific exceptions to package requirement decorations

--- polymerist/mdtools/openfftools/partialcharge/chargemethods.py
+++ polymerist/mdtools/openfftools/partialcharge/chargemethods.py
@@ -7,13 +7,12 @@ from typing import Type
 from collections import defaultdict
 
 from openff.toolkit.utils.base_wrapper import ToolkitWrapper
-from openff.toolkit.utils.rdkit_wrapper import RDKitToolkitWrapper
 from openff.toolkit.utils.builtin_wrapper import BuiltInToolkitWrapper
+from openff.toolkit.utils.rdkit_wrapper import RDKitToolkitWrapper
 from openff.toolkit.utils.openeye_wrapper import OpenEyeToolkitWrapper
 from openff.toolkit.utils.ambertools_wrapper import AmberToolsToolkitWrapper
 
-from openff import nagl_models
-from openff.nagl import GNNModel
+from openff.toolkit.utils.nagl_wrapper import NAGLToolkitWrapper
 from espaloma_charge.openff_wrapper import EspalomaChargeToolkitWrapper
 
 from .. import REGISTERED_TKWRAPPER_TYPES
@@ -53,9 +52,3 @@ for tkwrapper_type, supported_methods in SUPPORTED_PARTIAL_CHARGE_METHODS_BY_TOO
     if (tkwrapper_type in REGISTERED_TKWRAPPER_TYPES): # exclude non-registered toolkits to avoid confusion
         for method in supported_methods:
             TOOLKITS_BY_CHARGE_METHOD[method].append(tkwrapper_type)
-
-
-## NAGL GNN Model
-NAGL_MODEL_PATH = nagl_models.list_available_nagl_models()[1] # Path(/home/timber/miniconda3/envs/polymerist-env/lib/python3.11/site-packages/openff/nagl_models/models/openff-gnn-am1bcc-0.1.0-rc.1.pt)
-NAGL_MODEL_PATH = nagl_models.validate_nagl_model_path(NAGL_MODEL_PATH) # double check that this model path is still one of the valid entry point
-NAGL_MODEL = GNNModel.load(NAGL_MODEL_PATH)
