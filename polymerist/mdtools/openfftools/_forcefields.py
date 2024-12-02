b4afb52e5349f2d9ab459be8dd11699979ed9d89
Timotej Bernat <tibe3324@colorado.edu>
2024-12-02 16:00:17 -0700

    Fixed back-to-front imports for offxml getter functions

--- polymerist/mdtools/openfftools/_forcefields.py
+++ polymerist/mdtools/openfftools/_forcefields.py
@@ -12,14 +12,14 @@ from ...genutils.importutils.dependencies import modules_installed
 # Force field and ToolkitWrapper reference
 FFDIR : Optional[Path] = None
 if modules_installed('openff.toolkit'):
-    from openff.toolkit.typing.engines.smirnoff.forcefield import _get_installed_offxml_dir_paths
+    from openforcefields import get_forcefield_dirs_paths
     
     FFDIR = Path(get_forcefield_dirs_paths()[0]) # Locate path where OpenFF forcefields are installed
 
 FF_DIR_REGISTRY  : dict[Path, Path] = {}
 FF_PATH_REGISTRY : dict[Path, Path] = {}
 if modules_installed('openforcefields'):
-    from openforcefields import get_forcefield_dirs_paths
+    from openff.toolkit.typing.engines.smirnoff.forcefield import _get_installed_offxml_dir_paths
     
     for ffdir_str in _get_installed_offxml_dir_paths():
         ffdir = Path(ffdir_str)
