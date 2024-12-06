31f0675584d9ff623c7dfd20558f75932d9c6e4d
Timotej Bernat <tibe3324@colorado.edu>
2024-12-06 16:55:36 -0700

    Added Exception for unexpectedly-empty copolymer sequences

--- polymerist/polymers/exceptions.py
+++ polymerist/polymers/exceptions.py
@@ -3,7 +3,7 @@
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
-
+# CHAIN LENGTH AND SHAPE ERRORS
 class InsufficientChainLength(Exception):
     '''Raised when the polymer molecule being built is too short'''
     pass
@@ -15,14 +15,20 @@ class ExcessiveChainLength(Exception):
 class EndGroupDominatedChain(Exception):
     '''Raised to indicate there are more end groups present in a chain than are monomer possibly allowed'''
 
-class PartialBlockSequence(Exception):
-    '''Raised when an non-whole number of copolymer blocks is needed to reach a target chain length (and is not allowed)'''
-    pass
-
 class MorphologyError(Exception):
     '''Raised when a polymer does not have the morphology (i.e. crosslinking, molecular weight, etc) an application expects'''
     pass
 
+# COPOLYMER SEQUENCING ERRORS
+class EmptyBlockSequence(Exception):
+    '''Raised when a trivial sequence of copolymer block (i.e. the empty string "") is provided when no expected'''
+    pass
+
+class PartialBlockSequence(Exception):
+    '''Raised when an non-whole number of copolymer blocks is needed to reach a target chain length (and is not allowed)'''
+    pass
+
+# POLYMERIZATION MISINFORMATION ERRORS
 class AlreadySolvated(Exception):
     '''Raised when attempting to add solvent to a molecule which already has solvent'''
     pass
