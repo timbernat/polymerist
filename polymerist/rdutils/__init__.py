f42489bce2db96370a3ef66e0a1a981152a86d14
Timotej Bernat <tibe3324@colorado.edu>
2025-01-09 18:49:01 -0700

    Made rdkdraw setting accessible at rdutils toplevel

--- polymerist/rdutils/__init__.py
+++ polymerist/rdutils/__init__.py
@@ -2,3 +2,11 @@
 
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
+
+from .rdkdraw import (
+    set_rdkdraw_size,
+    enable_substruct_highlights,
+    disable_substruct_highlights,
+    enable_kekulized_drawing,
+    disable_kekulized_drawing,
+)
