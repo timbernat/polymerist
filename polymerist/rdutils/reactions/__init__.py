070962f2fe65e740c6c8a56d241fa9640de594da
Timotej Bernat <tibe3324@colorado.edu>
2025-01-30 21:14:38 -0700

    Made core reaction-related classes available at the package level

--- polymerist/rdutils/reactions/__init__.py
+++ polymerist/rdutils/reactions/__init__.py
@@ -2,3 +2,8 @@
 
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
+
+from .reactions import AnnotatedReaction, RxnProductInfo
+from .fragment import IBIS, IntermonomerBondIdentificationStrategy, ReseparateRGroupsUnique
+from .reactors import Reactor, PolymerizationReactor
+from .assembly import ReactionAssembler
