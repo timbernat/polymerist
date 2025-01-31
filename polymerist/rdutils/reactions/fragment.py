5382e92c5a10c6aa35b2e862e3224a7a01c1a311
Timotej Bernat <tibe3324@colorado.edu>
2025-01-30 21:11:24 -0700

    Fixed bug where heavy atom query was actually being initialized as aliphatic atom query

--- polymerist/rdutils/reactions/fragment.py
+++ polymerist/rdutils/reactions/fragment.py
@@ -14,6 +14,18 @@ from .reactions import RxnProductInfo
 from ..labeling import bondwise
 
 
+# HELPER FUNCTIONS
+HEAVY_FORMER_LINKER_QUERY_ATOM : Chem.QueryAtom = rdqueries.HasPropQueryAtom('was_dummy') # query for atoms with the property "was_dummy" set
+HEAVY_FORMER_LINKER_QUERY_ATOM.ExpandQuery(rdqueries.AAtomQueryAtom()) # expand query to only match heavy atoms
+
+def bridgehead_atom_ids(product : Chem.Mol) -> Generator[int, None, None]:
+    '''
+    Generates the indices of all atoms in a reaction product which were tagged
+    as R-group bridgehead (i.e. wild) atoms in the reaction template definition
+    '''
+    for bh_atom in product.GetAtomsMatchingQuery(HEAVY_FORMER_LINKER_QUERY_ATOM):
+        yield bh_atom.GetIdx()
+        
 # ABSTRACT BASE FOR FRAGMENTATION STRATEGIES
 class IntermonomerBondIdentificationStrategy(ABC):
     '''Abstract base for Intermonomer Bond Identification Strategies for fragmentation during in-silico polymerization'''
@@ -33,28 +45,21 @@ class IntermonomerBondIdentificationStrategy(ABC):
         return fragments # if separation is not requested, return as single unfragmented molecule object
 IBIS = IntermonomerBondIdentificationStrategy # shorthand alias for convenience
 
-# CONCRETE IMPLEMENTATIONS
-DUMMY_PROP_QUERY = rdqueries.HasPropQueryAtom('was_dummy') # heavy atom which was converted from a dummy atom in a reaction
-HEAVY_FORMER_LINKER_QUERY = Chem.MolFromSmarts('A')
-HEAVY_FORMER_LINKER_QUERY.GetAtomWithIdx(0).ExpandQuery(DUMMY_PROP_QUERY) # cast as Mol to allow for quick check via GetSubstructMatch
-
+## CONCRETE IMPLEMENTATIONS
 class ReseparateRGroups(IBIS):
     '''IBIS which cleaves any new bonds formed between atoms that were formerly the start of an R-group in the reaction template'''
     def locate_intermonomer_bonds(self, product: Mol, product_info : RxnProductInfo) -> Generator[int, None, None]:
-        possible_bridgehead_ids = [atom_id for match in product.GetSubstructMatches(HEAVY_FORMER_LINKER_QUERY) for atom_id in match]
-        for new_bond_id in product_info.new_bond_ids_to_map_nums.keys():                     # for each newly formed bond...
-            for bridgehead_id_pair in combinations(possible_bridgehead_ids, 2):                   # ...find the most direct path between bridgehead atoms...
-                if new_bond_id in bondwise.get_shortest_path_bonds(product, *bridgehead_id_pair): # ...and check if the new bond lies along it
+        for bridgehead_id_pair in combinations(bridgehead_atom_ids(product), 2):                         # for every pair of R-group bridgehead atoms...
+            for new_bond_id in product_info.new_bond_ids_to_map_nums.keys():                        # find the path(s) with fewest bonds between the bridgeheads... 
+                if new_bond_id in bondwise.get_shortest_path_bonds(product, *bridgehead_id_pair):   # and cut any newly-formed bonds along that path
                     yield new_bond_id
 
 class ReseparateRGroupsUnique(IBIS):
     '''IBIS which cleaves any new bonds formed between atoms that were formerly the start of an R-group in the reaction template exactly once each'''
     def locate_intermonomer_bonds(self, product: Mol, product_info : RxnProductInfo) -> Generator[int, None, None]:
-        possible_bridgehead_ids : list[int] = [atom_id for match in product.GetSubstructMatches(HEAVY_FORMER_LINKER_QUERY) for atom_id in match]
         bonds_already_cut : set[int] = set()
-
-        for bridgehead_id_pair in combinations(possible_bridgehead_ids, 2):                                     # for every pair of R-group bridgehead atoms...
-            shortest_path_bond_ids : list[int] = bondwise.get_shortest_path_bonds(product, *bridgehead_id_pair) # find the shortest bond path between the bridgeheads... 
+        for bridgehead_id_pair in combinations(bridgehead_atom_ids(product), 2):                                     # for every pair of R-group bridgehead atoms...
+            shortest_path_bond_ids : list[int] = bondwise.get_shortest_path_bonds(product, *bridgehead_id_pair) # find the path(s) with fewest bonds between the bridgeheads... 
             for new_bond_id in product_info.new_bond_ids_to_map_nums.keys():                                    # then for each newly formed bond...
                 if (new_bond_id in shortest_path_bond_ids) and (new_bond_id not in bonds_already_cut):          # if the new bond lies along the path and has not already been selected for cutting...
                     yield new_bond_id                                                                           # select the new bond for cutting...
