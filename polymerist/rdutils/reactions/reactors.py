2ea8dcd7fbf60da167b4ebad25e76f43bc78a5fb
Timotej Bernat <tibe3324@colorado.edu>
2025-03-03 19:24:58 -0700

    Implemented iterative variant of propagation algorithm

--- polymerist/rdutils/reactions/reactors.py
+++ polymerist/rdutils/reactions/reactors.py
@@ -21,7 +21,7 @@ from ..rdprops.bondprops import clear_bond_props
 from ..chemlabel import clear_atom_map_nums
 from ..sanitization import sanitize
 
-from ...smileslib.cleanup import canonical_SMILES_from_mol
+from ...smileslib.cleanup import canonical_SMILES_from_mol, Smiles
 
 
 @dataclass
@@ -33,7 +33,7 @@ class PolymerizationReactor:
     def propagate(
         self,
         monomers : Sequence[Mol],
-        clear_map_nums : bool=True,
+        clear_map_labels : bool=True,
         sanitize_ops : SanitizeFlags=SANITIZE_ALL,
         aromaticity_model : AromaticityModel=AROMATICITY_RDKIT,
      ) -> Generator[tuple[tuple[Mol], tuple[Mol]], None, None]:
@@ -58,8 +58,8 @@ class PolymerizationReactor:
                     sanitize(fragment, sanitize_ops=sanitize_ops, aromaticity_model=aromaticity_model, in_place=True) # apply same cleanup ops to fragments as to adduct
                     fragments.append(fragment)
 
-                if clear_map_nums: # NOTE : CRITICAL that this be done after fragmentation step, which RELIES on map numbers being present
-                    clear_atom_map_nums(adduct, in_place=True)
+                if clear_map_labels: # NOTE : CRITICAL that this be done after fragmentation step, which RELIES on map numbers being present
+                    clear_atom_map_nums(adduct)
                 adducts.append(adduct)
                     
             yield tuple(adducts), tuple(fragments) # yield the adduct Mol and any subsequent resulting reactive fragments
@@ -68,8 +68,9 @@ class PolymerizationReactor:
     def propagate_pooled(
             self,
             monomers : Sequence[Mol],
-            n_rxn_steps_max : int=5,
-            clear_map_nums : bool=True,
+            rxn_depth_max : int=5,
+            allow_resampling : bool=False,
+            clear_map_labels : bool=True,
             sanitize_ops : SanitizeFlags=SANITIZE_ALL,
             aromaticity_model : AromaticityModel=AROMATICITY_RDKIT,
         ) -> None:
@@ -78,8 +79,58 @@ class PolymerizationReactor:
         
         Propagation acts on a pool of fragments reactants (initially just the "monomers" passed in) and proceeds in rounds
         where all reactible subsets are adducted and fragmented, and any previously-unseen fragments are added to the pool
-        Enumeration halts either when no new fragments have been, or the set maximum number of reaction steps is reached
+        Enumeration halts either when no new fragments have been, or the set maximum number of reaction step(s) is reached
         
         "Uniqueness" of a fragment is assessed by its RDKit-canonicalized SMILES representation
         '''
-        ...
+        reactant_pool : dict[Smiles, Mol] = { # initialize reactants as just the monomers provided
+            canonical_SMILES_from_mol(monomer) : monomer
+                for monomer in monomers
+        } # TODO: add option for removing initial reactants from pool at end of process
+        
+        rxn_depth : int = 1
+        while (rxn_depth <= rxn_depth_max):
+            LOGGER.info(f'Enumerating fragments formable in {rxn_depth} reaction step(s) or fewer:')
+            n_new_frags_found : int = 0 # this is reassigned to reset the count to 0 each round
+            for reactants in self.rxn_schema.enumerate_valid_reactant_orderings(
+                reactant_pool=reactant_pool, # DEVNOTE: some room for optimization here down the line in avoiding checking seen combinations up-front
+                labeling_method=canonical_SMILES_from_mol, # NOTE: reactant indices would be enough for uniqueness, but chemical labels allow for tracing identity between steps
+                as_mols=True, # explicitly WANT to return canonical SMILES labels, not molecule objects  
+                allow_resampling=allow_resampling,
+                deterministic=True,
+            ):
+                if reactants is None:
+                    break # NOTE: this ONLY exists the reactants loop, NOT the encapsulating while loop
+                
+                for adduct in self.rxn_schema.react(
+                    reactants,
+                    repetitions=1,
+                    keep_map_labels=True, # can't clear map numbers yet, otherwise intermonomer bond finder would have nothing to work with
+                    sanitize_ops=sanitize_ops,
+                    aromaticity_model=aromaticity_model,
+                    _suppress_reactant_validation=True, # avoid double-validation since we required it as a precheck for this protocol
+                ):
+                    for fragment in self.fragment_strategy.produce_fragments(adduct, separate=True):
+                        clear_atom_props(fragment, in_place=True) # essential to avoid reaction mapping info from prior steps from contaminating future ones
+                        clear_bond_props(fragment, in_place=True)
+                        sanitize(fragment, sanitize_ops=sanitize_ops, aromaticity_model=aromaticity_model, in_place=True) # apply same cleanup ops to fragments as to adduct
+                        
+                        if clear_map_labels: # NOTE : CRITICAL that this be done after fragmentation step, which RELIES on map numbers being present
+                            clear_atom_map_nums(fragment)
+                                
+                        if (canon_smi := canonical_SMILES_from_mol(fragment)) not in reactant_pool:
+                            LOGGER.debug(f'Discovered new fragment with canonical SMILES "{canon_smi}"')
+                            reactant_pool[canon_smi] = fragment
+                            n_new_frags_found += 1
+            
+            # summarizing current rxn round and checking halting conditions
+            LOGGER.info(f'Found {n_new_frags_found} new fragments formable after at least {rxn_depth} reaction step(s)')
+            if n_new_frags_found == 0:
+                LOGGER.info(f'HALTING NORMALLY: No new reaction fragments discovered requiring more than {rxn_depth} reaction step(s)')
+                break
+            rxn_depth += 1
+            
+        else: # only called if fragment loop is not broken out of (i.e. when while condition on rxn depth becomes Falsy)
+            LOGGER.warning(f'HALTING PREMATURELY: reached the configured reaction search depth limit of {rxn_depth_max} reaction step(s)')
+        
+        return reactant_pool
