f336ea5605bd089b5241a51e31aae3601afa23ec
Timotej Bernat <tibe3324@colorado.edu>
2025-05-02 17:42:13 -0600

    Added some example SMILES with multiple functional groups for propagation testing

--- polymerist/tests/rdutils/reactions/test_propagation.py
+++ polymerist/tests/rdutils/reactions/test_propagation.py
@@ -16,4 +16,10 @@ from . import RXNS
 def test_reactant_smiles() -> dict[str, str]:
     # TOSELF: Ought to include at least one example of the many logical edge cases I'm trying to
     # handle (e.g. autopolymerization, large monomers, more than 2 monomers, etc.)
+    multifunct_examples : list[str] = [
+        'NCCCCCC(=O)O.NCCCCCCN.O=C(O)CCCCC(=O)O',
+        'NCCCCCC(=O)O.NCCCCCCN.O=C(O)c1ccc(C(=O)O)cc1',
+        'CC(C)(c1ccc(O)cc1)c1ccc(O)cc1.O=C(Cl)Cl.Oc1ccc(Oc2ccc(O)cc2)cc1',
+        'O=C(O)CCC(=O)O.O=C(O)c1ccc(C(=O)O)o1.OCCCCO',
+    ]
     ...
