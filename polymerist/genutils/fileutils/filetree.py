30d6dcd08fce5587acf8186e08cd0032113725a9
Timotej Bernat <tibe3324@colorado.edu>
2025-05-08 16:37:16 -0600

    Added a utility for temporarily changing the current working directory within a context

--- polymerist/genutils/fileutils/filetree.py
+++ polymerist/genutils/fileutils/filetree.py
@@ -3,10 +3,14 @@
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
 
-from typing import Iterable
+from typing import Generator, Iterable, Optional
 
-from pathlib import Path
 from subprocess import Popen
+from contextlib import contextmanager
+
+from os import getcwd, chdir
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from ..trees.treebase import NodeCorrespondence, compile_tree_factory
 from ..trees.treeviz import treestr
@@ -44,6 +48,24 @@ dir_tree = allow_string_paths(
 def startfile(path : Path) -> None:
     '''Replacement for os.startfile() functionality, since none natively exists in Linux'''
     Popen(['xdg-open', path])
+
+@contextmanager
+@allow_string_paths # TODO: allow dir_path to be entirely optional (@allow_string_paths currently does not support this)
+def temporary_cd(dir_path : Optional[Path]) -> Generator[Path, None, None]: 
+    '''Context manager for temporarily changing the current working directory of code wrapped in a "with" block'''
+    original_cwd : str = getcwd()
+    try:
+        if dir_path:
+            if not dir_path.is_dir():
+                dir_path.mkdir()
+            chdir(dir_path)
+            yield dir_path
+        else:
+            with TemporaryDirectory() as temp_dir:
+                chdir(temp_dir)
+                yield Path(temp_dir)
+    finally:
+        chdir(original_cwd)
 
 def clear_dir(path : Path) -> None:
     '''Recursively clear contents of a directory at the given path (depth-first)'''
