773ca752e983292efa14fcb2b09880b8b5633a9d
Timotej Bernat <tibe3324@colorado.edu>
2025-05-08 17:34:56 -0600

    Deprecated superfluous (and unused) numpy vector typehints

--- polymerist/genutils/typetools/numpytypes.py
+++ polymerist/genutils/typetools/numpytypes.py
@@ -28,12 +28,3 @@ M = TypeVar('M', bound=int) # for typing arbitrary array dimension size
 
 DType = TypeVar('DType', bound=np.generic)
 Shape : TypeAlias = tuple
-
-# Vectory type hinting
-Coords2D : TypeAlias = np.ndarray[Shape[M, 2], DType] # an Mx2 array of M 3D coordinates
-Coords3D : TypeAlias = np.ndarray[Shape[M, 3], DType] # an Mx3 array of M 3D coordinates
-CoordsND : TypeAlias = np.ndarray[Shape[M, N], DType] # an MxN array of M ND coordinates
-
-Vector2D : TypeAlias = np.ndarray[Shape[2], DType] # a 1x2 array of M 3D coordinates
-Vector3D : TypeAlias = np.ndarray[Shape[3], DType] # a 1x3 array of M 3D coordinates
-VectorND : TypeAlias = np.ndarray[Shape[N], DType] # a 1xN array of M ND coordinates
