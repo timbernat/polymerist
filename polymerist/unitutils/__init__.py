e21054ecb1e7ca7787e0d45dae09e147f66bb25c
Timotej Bernat <tibe3324@colorado.edu>
2025-07-21 21:19:08 -0600

    Made unitutils.dimensions utils importable at toplevel (omitted extraunits and physconsts due to dynamic init in either)

--- polymerist/unitutils/__init__.py
+++ polymerist/unitutils/__init__.py
@@ -2,3 +2,11 @@
 
 __author__ = 'Timotej Bernat'
 __email__ = 'timotej.bernat@colorado.edu'
+
+from .dimensions import(
+    MissingUnitsError,
+    hasunits,
+    strip_units,
+    is_volume,
+    quantities_approx_equal,
+)
