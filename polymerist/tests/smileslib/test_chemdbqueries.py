76e7097f30cdf2828c1ae7cbfaa3c36af6dc66ab
timbernat <tibe3324@colorado.edu>
2026-08-11 16:56:03 -0600

    Encapsulated test examples in fixture-like functions, clarified typehints

--- polymerist/tests/smileslib/test_chemdbqueries.py
+++ polymerist/tests/smileslib/test_chemdbqueries.py
@@ -18,8 +18,8 @@ from polymerist.smileslib.chemdbqueries import (
     NullPropertyResponse,
     ChemicalDataQueryFailed,
     ChemDBServiceQueryStrategy,
-    # NOTE: these strageies are implemented to be defined even if the packages in question aren't installed
-    # it's just that instances will raise excception on most of their method calls
+    # NOTE: these strategies are defined even if their dependencies aren't installed
+    # their object instances will just raise Exception on most of their method calls
     NIHCACTUSQueryStrategy,
     PubChemQueryStrategy,
     get_chemical_property,
@@ -38,7 +38,7 @@ for ChemDBStrategy in ChemDBServiceQueryStrategy.__subclasses__(): # dynamically
     CHEMDB_STRATEGY_ONLINE[          ChemDBStrategy] = ChemDBStrategy.is_online()
     CHEMDB_STRATEGY_DEPENDENCIES_MET[ChemDBStrategy] = modules_installed(*ChemDBStrategy.dependencies())
     
-def skip_pytest_on_invalid_service(service_type : ChemDBServiceQueryStrategy) -> None:
+def skip_pytest_on_invalid_service(service_type : type[ChemDBServiceQueryStrategy]) -> None:
     '''
     Boilerplate function for skipping a test if the requested database
     query service is either missing local dependencies or is offline
@@ -77,228 +77,230 @@ class ChemDBQueryParameters:
     keep_first_only : bool
     allow_null_return : bool
 
-# examples which test
-ETHANOL_PARAMS = ChemDBQueryParameters(
-    identifier='CCO',
-    namespace='smiles',
-    keep_first_only=True,
-    allow_null_return=True, 
-)
-FIXED_PARAMETER_EXAMPLES : list[tuple[str, type[ChemDBServiceQueryStrategy], ChemDBQueryParameters, Any]] = [
-    (property_name, strategy_type, ETHANOL_PARAMS)
-        for strategy_type, dependencies_met in CHEMDB_STRATEGY_DEPENDENCIES_MET.items()
-            if dependencies_met
-                for property_name in strategy_type.queryable_properties()
-]
+def ethanol_params() -> ChemDBQueryParameters:
+    '''Fixed chemical query inputs for ethanol'''
+    return ChemDBQueryParameters(
+        identifier='CCO',
+        namespace='smiles',
+        keep_first_only=True,
+        allow_null_return=True, 
+    )
+
+def fixed_parameter_examples(chem_input : ChemDBQueryParameters) -> list[
+    tuple[str, type[ChemDBServiceQueryStrategy], ChemDBQueryParameters]
+]:
+    '''Examples which test all queryable properties against a fixed chemical input'''
+    return [
+        (property_name, strategy_type, chem_input)
+            for strategy_type, dependencies_met in CHEMDB_STRATEGY_DEPENDENCIES_MET.items()
+                if dependencies_met
+                    for property_name in strategy_type.queryable_properties()
+    ]
 
 # examples which test that many diverse inputs yield expected outputs
-VARIED_PARAMETER_EXAMPLES : list[
-    tuple[
-        str,
-        type[ChemDBServiceQueryStrategy],
-        ChemDBQueryParameters,
-        Any,
-    ] | ParameterSet
-] = [
-    # for NIH CACTUS
-    ( ## simple queries known to work for all services
-        'iupac_name',
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters( 
-            identifier='CCO',
-            namespace='smiles',
-            keep_first_only=True,
-            allow_null_return=False,
+def varied_parameter_examples() -> list[
+    tuple[str, type[ChemDBServiceQueryStrategy], ChemDBQueryParameters, Any] | ParameterSet
+]:
+    return [
+        # for NIH CACTUS
+        ( ## simple queries known to work for all services
+            'iupac_name',
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters( 
+                identifier='CCO',
+                namespace='smiles',
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            'ethanol'
         ),
-        'ethanol'
-    ),
-    (
-        'inchi',
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters( 
-            identifier='N-methylformamide',
-            namespace='name',
-            keep_first_only=True,
-            allow_null_return=False,
+        (
+            'inchi',
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters( 
+                identifier='N-methylformamide',
+                namespace='name',
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            'InChI=1/C2H5NO/c1-3-2-4/h2H,1H3,(H,3,4)/f/h3H',
         ),
-        'InChI=1/C2H5NO/c1-3-2-4/h2H,1H3,(H,3,4)/f/h3H',
-    ),
-    ( ## testing that different namespaces can be queried
-        'mw',
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='benzophenone',
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=False,
+        ( ## testing that different namespaces can be queried
+            'mw',
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='benzophenone',
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            '182.2214'
         ),
-        '182.2214'
-    ),
-    ( ## testing that returns with multiple data values work
-        'names',
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='c1ccccc1-C(=S)S',
-            namespace='smiles', 
-            keep_first_only=False, 
-            allow_null_return=False,
+        ( ## testing that returns with multiple data values work
+            'names',
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='c1ccccc1-C(=S)S',
+                namespace='smiles', 
+                keep_first_only=False, 
+                allow_null_return=False,
+            ),
+            ['Benzenecarbodithioic acid', '121-68-6', 'UPCMLD00WV-104', 'EINECS 204-491-4', 'Benzenecarbodithioic acid', 'Dithiobenzoic acid', 'NSC732246']
         ),
-        ['Benzenecarbodithioic acid', '121-68-6', 'UPCMLD00WV-104', 'EINECS 204-491-4', 'Benzenecarbodithioic acid', 'Dithiobenzoic acid', 'NSC732246']
-    ),
-    ( ## testing that enabling and disabling None returns is handled properly in both cases
-        'inchi',
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='bogus-name', # this is obviously fake and should not return anything
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=True,
+        ( ## testing that enabling and disabling None returns is handled properly in both cases
+            'inchi',
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='bogus-name', # this is obviously fake and should not return anything
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=True,
+            ),
+            None
         ),
-        None
-    ),
-    pytest.param( 
-        'inchi',
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='bogus-name', # this is obviously fake and should not return anything
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=False,
+        pytest.param( 
+            'inchi',
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='bogus-name', # this is obviously fake and should not return anything
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            None,
+            marks=pytest.mark.xfail(
+                raises=(NullPropertyResponse, ChemicalDataQueryFailed),
+                reason='Did not allow response to be NoneType',
+                strict=True,
+            )
         ),
-        None,
-        marks=pytest.mark.xfail(
-            raises=(NullPropertyResponse, ChemicalDataQueryFailed),
-            reason='Did not allow response to be NoneType',
-            strict=True,
-        )
-    ),
-    pytest.param( ## testing that invalid property values are caught before attempting a query
-        'in_no_way_a_valid_property', # this should not even be considered a valid property
-        NIHCACTUSQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='benophenone', 
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=False,
+        pytest.param( ## testing that invalid property values are caught before attempting a query
+            'in_no_way_a_valid_property', # this should not even be considered a valid property
+            NIHCACTUSQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='benophenone', 
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            None,
+            marks=pytest.mark.xfail(
+                raises=(InvalidPropertyError, ChemicalDataQueryFailed),
+                reason='Tried to query a property that does not exist',
+                strict=True,
+            )
         ),
-        None,
-        marks=pytest.mark.xfail(
-            raises=(InvalidPropertyError, ChemicalDataQueryFailed),
-            reason='Tried to query a property that does not exist',
-            strict=True,
-        )
-    ),
-    
-    # for PubChem
-    ( ## simple queries known to work for all services
-        'iupac_name',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='CCO',
-            namespace='smiles',
-            keep_first_only=True,
-            allow_null_return=False
+        
+        # for PubChem
+        ( ## simple queries known to work for all services
+            'iupac_name',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='CCO',
+                namespace='smiles',
+                keep_first_only=True,
+                allow_null_return=False
+            ),
+            'ethanol'
         ),
-        'ethanol'
-    ),
-    (
-        'inchi',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters( 
-            identifier='N-methylformamide',
-            namespace='name',
-            keep_first_only=True,
-            allow_null_return=False,
+        (
+            'inchi',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters( 
+                identifier='N-methylformamide',
+                namespace='name',
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            'InChI=1S/C2H5NO/c1-3-2-4/h2H,1H3,(H,3,4)',
         ),
-        'InChI=1S/C2H5NO/c1-3-2-4/h2H,1H3,(H,3,4)',
-    ),
-    ( ## testing that different namespaces can be queried
-        'MolecularWeight',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='InChI=1S/C2H5NO/c1-3-2-4/h2H,1H3,(H,3,4)',
-            namespace='inchi', 
-            keep_first_only=True,
-            allow_null_return=False,
+        ( ## testing that different namespaces can be queried
+            'MolecularWeight',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='InChI=1S/C2H5NO/c1-3-2-4/h2H,1H3,(H,3,4)',
+                namespace='inchi', 
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            '59.07',
         ),
-        '59.07',
-    ),
-    ( ## testing that returns with multiple data values work
-        'HeavyAtomCount',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='CCO', 
-            namespace='smiles', 
-            keep_first_only=False,
-            allow_null_return=False,
+        ( ## testing that returns with multiple data values work
+            'HeavyAtomCount',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='CCO', 
+                namespace='smiles', 
+                keep_first_only=False,
+                allow_null_return=False,
+            ),
+            [3], # note that this is wrapped in a list, as are all PubChem queries by deualt; I couldn't find a good example which returns more than one value like cirpy does
         ),
-        [3], # note that this is wrapped in a list, as are all PubChem queries by deualt; I couldn't find a good example which returns more than one value like cirpy does
-    ),
-    pytest.param( ## testing sending malformed queries to PubChem
-        'inchi',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='bogus-name', # this is obviously fake and should not return anything
-            namespace='smiles', 
-            keep_first_only=True,
-            allow_null_return=True,
+        pytest.param( ## testing sending malformed queries to PubChem
+            'inchi',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='bogus-name', # this is obviously fake and should not return anything
+                namespace='smiles', 
+                keep_first_only=True,
+                allow_null_return=True,
+            ),
+            None,
+            marks=pytest.mark.xfail(
+                raises=(ChemicalDataQueryFailed, HTTPError), # DEVNOTE: HTTPError here is absolutely necessary! Request should return HTTP error 400
+                reason='Invalid request sent to PubChem (queried a name as a SMILES string)',
+                strict=True,
+            )
         ),
-        None,
-        marks=pytest.mark.xfail(
-            raises=(ChemicalDataQueryFailed, HTTPError), # DEVNOTE: HTTPError here is absolutely necessary! Request should return HTTP error 400
-            reason='Invalid request sent to PubChem (queried a name as a SMILES string)',
-            strict=True,
-        )
-    ),
-    ( ## testing that enabling and disabling None returns is handled properly in both cases
-        'inchi',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='bogus-name', # this is obviously fake and should not return anything
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=True,
+        ( ## testing that enabling and disabling None returns is handled properly in both cases
+            'inchi',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='bogus-name', # this is obviously fake and should not return anything
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=True,
+            ),
+            None,
         ),
-        None,
-    ),
-    pytest.param(  
-        'inchi',
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='bogus-name', # this is obviously fake and should not return anything
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=False,
+        pytest.param(  
+            'inchi',
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='bogus-name', # this is obviously fake and should not return anything
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            None,
+            marks=pytest.mark.xfail(
+                raises=(NullPropertyResponse, ChemicalDataQueryFailed),
+                reason='Did not allow response to be NoneType',
+                strict=True,
+            )
         ),
-        None,
-        marks=pytest.mark.xfail(
-            raises=(NullPropertyResponse, ChemicalDataQueryFailed),
-            reason='Did not allow response to be NoneType',
-            strict=True,
-        )
-    ),
-    pytest.param( ## testing that invalid property values are caught before attempting a query
-        'in_no_way_a_valid_property', # this should not even be considered a valid property
-        PubChemQueryStrategy,
-        ChemDBQueryParameters(
-            identifier='benophenone', 
-            namespace='name', 
-            keep_first_only=True,
-            allow_null_return=False,
+        pytest.param( ## testing that invalid property values are caught before attempting a query
+            'in_no_way_a_valid_property', # this should not even be considered a valid property
+            PubChemQueryStrategy,
+            ChemDBQueryParameters(
+                identifier='benophenone', 
+                namespace='name', 
+                keep_first_only=True,
+                allow_null_return=False,
+            ),
+            None,
+            marks=pytest.mark.xfail(
+                raises=(InvalidPropertyError, ChemicalDataQueryFailed),
+                reason='Tried to query a property that does not exist',
+                strict=True,
+            )
         ),
-        None,
-        marks=pytest.mark.xfail(
-            raises=(InvalidPropertyError, ChemicalDataQueryFailed),
-            reason='Tried to query a property that does not exist',
-            strict=True,
-        )
-    ),
-]
+    ]
     
 class TestChemicalDatabaseServiceQueries:
     @pytest.mark.parametrize(
         'property_name,service_type,query_params',
-        FIXED_PARAMETER_EXAMPLES,
+        fixed_parameter_examples(ethanol_params()),
     )
     def test_queryable_properties(
         self,
@@ -322,7 +324,7 @@ class TestChemicalDatabaseServiceQueries:
 
     @pytest.mark.parametrize(
         'property_name,service_type,query_params,expected_property',
-        VARIED_PARAMETER_EXAMPLES,
+        varied_parameter_examples(),
     )
     def test_direct_service_property_query(
         self,
@@ -350,7 +352,7 @@ class TestChemicalDatabaseServiceQueries:
         
     @pytest.mark.parametrize(
         'property_name,service_type,query_params,expected_property',
-        VARIED_PARAMETER_EXAMPLES,
+        varied_parameter_examples(),
     )
     def test_get_chemical_property_wrapper(
         self,
